03aea9a9e2279c7ed688e2078227a38ca41669a3
noftaly <34779161+noftaly@users.noreply.github.com>
2019-04-13 23:42:12 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable max-statements */
+
 import 'module-alias/register';
 
 import Config from '../config/config.json';
 import app from './app';
-import to from '@helpers/To';
+import to from "@helpers/To";
 
 const App =  new app(Config);
 
@@ -15,20 +18,41 @@ const App =  new app(Config);
 	[err, discord] = await to(App.loadDiscord());
 	if (err) throw err;
 
+	// eslint-disable-next-line no-console
 	console.log('Client discord lancé');
 
 	discord.on('message', async payload => {
 
-		if (payload.author.bot || payload.channel.type === 'dm' || payload.channel.id !== Config.bot.channel) return;
+		if (payload.author.bot) return;
+		if (payload.channel.type === 'dm') return;
+		if (payload.channel.id !== Config.bot.channel) return;
 
 		const message = payload.content.toLowerCase().split(" ");
+
 		for (let command of commands) {
 
-			const search = message[0].search(`^${Config.bot.prefix}${command.regex}`);
-			if (search === -1) continue;
+			const search = message[0].search(new RegExp(`^${Config.bot.prefix}${command.regex.source}`, command.regex.flags));
+			let hasRole = false;
 
-			await command.execute(payload, message.splice(1));
-			return;
+			if (search === -1) continue;
+			if (command.permissions.length > 0) {
+				for (let index = 0; index < command.permissions.length; index += 1) {
+					if (payload.member.roles.find(role => role.name === `${command.permissions[index]}`)) {
+						hasRole = true;
+						break;
+					}
+				}
+			} else {
+				hasRole = true;
+			}
+
+			if (hasRole) {
+				await command.execute(payload, message.splice(0, 1), message.splice(0, message.length));
+			} else {
+				payload.reply('Vous n\'avez pas la permission d\'executer cette commande.');
+			}
+
+			break;
 		}
 	});
 })();
