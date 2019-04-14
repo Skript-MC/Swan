91a2122612e24817c4626d2432c8014553d19578
noftaly <34779161+noftaly@users.noreply.github.com>
2019-04-14 20:27:23 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,58 +1,51 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable max-statements */
+import Discord from 'discord.js';
+import readdir from 'fs-readdir-promise';
+import fs from 'fs-extra';
 
-import 'module-alias/register';
+import to from '@helpers/To';
 
-import Config from '../config/config.json';
-import app from './app';
-import to from "@helpers/To";
+export default class {
 
-const App =  new app(Config);
+	constructor(config) {
+		this.config = config;
+	}
 
-(async function f() {
+	async loadDiscord() {
+		const discord = new Discord.Client();
+		discord.login(this.config.bot.token);
 
-	let err, commands, discord;
+		return discord;
+	}
 
-	[err, commands] = await to(App.loadCommands());
-	if (err) throw err;
-	[err, discord] = await to(App.loadDiscord());
-	if (err) throw err;
+	async loadPlaylists() {
+		const playlist = [];
 
-	// eslint-disable-next-line no-console
-	console.log('Client discord lancé');
+		let [err, files] = await to(readdir('./playlists'));
+		if (err) throw err;
 
-	discord.on('message', async payload => {
+		for (let file of files) {
+			[err, file] = await to(fs.readJson(`./playlists/${file}`));
+			if (err) throw err;
 
-		if (payload.author.bot) return;
-		if (payload.channel.type === 'dm') return;
-		if (payload.channel.id !== Config.bot.channel) return;
-
-		const message = payload.content.toLowerCase().split(" ");
-
-		for (let command of commands) {
-
-			const search = message[0].search(new RegExp(`^${Config.bot.prefix}${command.regex.source}`, command.regex.flags));
-			let hasRole = false;
-
-			if (search === -1) continue;
-			if (command.permissions.length > 0) {
-				for (let index = 0; index < command.permissions.length; index += 1) {
-					if (payload.member.roles.find(role => role.name === `${command.permissions[index]}`)) {
-						hasRole = true;
-						break;
-					}
-				}
-			} else {
-				hasRole = true;
-			}
-
-			if (hasRole) {
-				await command.execute(payload, message.splice(0, 1), message.splice(0, message.length));
-			} else {
-				payload.reply('Vous n\'avez pas la permission d\'executer cette commande.');
-			}
-
-			break;
+			playlist.push(file);
 		}
-	});
-})();
+
+		return playlist;
+	}
+
+	async loadCommands() {
+		const commands = [];
+
+		let [err, files] = await to(readdir('./src/commands'));
+		if (err) throw err;
+
+		for (let file of files) {
+			// eslint-disable-next-line no-undef
+			const module = require(`@commands/${file}`);
+			if (err) throw err;
+			commands.push(module.default);
+		}
+
+		return commands;
+	}
+}
