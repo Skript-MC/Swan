03c9f0f66f7c6540dfbe4c35e38b0e37e6bab647
noftaly <34779161+noftaly@users.noreply.github.com>
2019-06-26 23:58:41 +0200

    Create serverInfo.js

---
+++ src/commands/info/serverInfo.js
@@ -0,0 +1,58 @@
+import { RichEmbed } from "discord.js";
+import Command from '../../components/Command';
+import { discordError } from '../../components/Messages';
+import config from "../../../config/config.json";
+import axios from 'axios';
+
+class ServerInfo extends Command {
+	constructor() {
+		super('serverinfo')
+		this.usage = `${config.bot.prefix}serveur-info <IP>`;
+		this.examples = ['serv-info hypixel.net'];
+		this.regex = /se?rv(?:eu?r)?-?info(?:rmation)?s?/gmui;
+	}
+
+	async execute(message, args) {
+		let msg = await message.channel.send("Je vais chercher ça...");
+		const data = await axios(`${config.miscellaneous.api_server}${args[0]}`, { method: 'GET' })
+			.then(async response => {
+				if (response.status !== 200) {
+					console.error(`[HTTP request failed] Error : ${response.status}`);
+					return discordError(`Une erreur est survenue lors de la reqûete... Veuillez réessayer plus tard.\nStatus de la requête : ${response.status}`, message)
+				}
+				return response.data;
+			}).catch(err => console.error(err));
+
+		if (!data) return discordError("Aucun serveur ne correspond a votre reqûete !", message);
+		return this.sendDetails(message, msg, data, args[0]);
+	}
+
+	sendDetails(message, msg, data, ip) {
+		const embed = new RichEmbed()
+			.setColor(config.bot.color)
+			.setAuthor(`Informations sur ${ip}`, config.bot.avatar)
+			.setFooter(`Executé par ${message.author.username} | Données fournies par https://api.mcsrvstat.us/`)
+			.setTimestamp();
+
+		if (data.online != undefined)
+			embed.addField(this.config.embed.status, (data.online ? "En ligne" : "Hors ligne"), true);
+		if (data.ip)
+			embed.addField(this.config.embed.ip, `\`${data.ip}${data.port ? `:${data.port}` : ""}\``, true);
+		if (data.players)
+			embed.addField(this.config.embed.players, `${data.players.online}/${data.players.max}`, true);
+		if (data.version)
+			embed.addField(this.config.embed.version, data.version, true);
+		if (data.hostname)
+			embed.addField(this.config.embed.hostname, data.hostname, true);
+		if (data.software)
+			embed.addField(this.config.embed.software, data.software, true);
+		if (data.plugins)
+			embed.addField(this.config.embed.plugins, data.plugin.raw.length, true);
+		if (data.mods)
+			embed.addField(this.config.embed.mods, data.mods.raw.length, true);
+
+		msg.edit(embed);
+	}
+};
+
+export default ServerInfo;
