9c56d47d2f8b5bf0418768f996c6ffb3663d6f12
noftaly <34779161+noftaly@users.noreply.github.com>
2019-06-29 13:37:01 +0200

    Updated all commands

--- src/commands/basic/toggleNotifRole.js
+++ src/commands/basic/toggleNotifRole.js
@@ -2,40 +2,38 @@ import Command from '../../components/Command';
 import { config } from '../../main';
 
 class ToggleNotifRole extends Command {
+  constructor() {
+    super('Toggle role notification');
+    this.usage = 'toggle-notif-role';
+    this.examples.push('toggle-notif-role');
+    this.regex = /(add|give|ask|toggle|remove)(-|_)?notif(ication)?(-|_)?role/gmui;
+  }
 
-	constructor () {
-		super('Toggle role notification');
-		this.usage = 'toggle-notif-role'
-		this.example = 'toggle-notif-role'
-		this.regex = /(add|give|ask|toggle|remove)(-|_)?notif(ication)?(-|_)?role/gmui;
-	}
+  async execute(message, _args) {
+    message.delete();
+    const rolename = config.miscellaneous.notifRoleName;
+    let role = message.guild.roles.find(r => r.name === rolename);
+    if (!role) {
+      try {
+        role = await message.guild.createRole({
+          permissions: [],
+          name: rolename,
+          mentionable: false,
+        });
+      } catch (err) {
+        console.error('Error while attempting to create the role:');
+        console.error(err);
+      }
+    }
 
-	async execute(message, args) {
-		message.delete();
-		const rolename = config.miscellaneous.notifRoleName
-		let role = message.guild.roles.find(r => r.name === rolename);
-		if (!role) {
-			try {
-				role = await message.guild.createRole({
-					permissions: [],
-					name: rolename,
-					mentionable: false
-				});
-			} catch (err) {
-				console.error(`Error while attempting to create the role:`);
-				console.error(err);
-			}
-		}
-
-		if (!message.member.roles.has(role.id)) {
-			await message.member.addRole(role);
-			message.member.send(`**(${message.guild})** Le rôle *"${rolename}"* vous a été ajouté !`);
-		} else if (message.member.roles.has(role.id)) {
-			await message.member.removeRole(role);
-			message.member.send(`**(${message.guild})** Le rôle *"${rolename}"* vous a été enlevé !`);
-		}
-	}
-
+    if (!message.member.roles.has(role.id)) {
+      await message.member.addRole(role);
+      message.member.send(`**(${message.guild})** Le rôle *"${rolename}"* vous a été ajouté !`);
+    } else if (message.member.roles.has(role.id)) {
+      await message.member.removeRole(role);
+      message.member.send(`**(${message.guild})** Le rôle *"${rolename}"* vous a été enlevé !`);
+    }
+  }
 }
 
 export default ToggleNotifRole;
--- src/commands/info/playerInfos.js
+++ src/commands/info/playerInfos.js
@@ -1,100 +1,78 @@
-import { RichEmbed } from "discord.js";
+/* eslint-disable consistent-return */
+import { RichEmbed } from 'discord.js';
+import axios from 'axios';
 import Command from '../../components/Command';
 import { discordError } from '../../components/Messages';
-import config from "../../../config/config.json";
-import { formatDate } from "../../utils";
-import fetch from 'node-fetch';
+import { config } from '../../main';
+import { formatDate } from '../../utils';
 
 class PlayerInfos extends Command {
-	constructor() {
-		super('playerinfo')
-		this.usage = `${config.bot.prefix}player-info <pseudo>`;
-		this.examples = ['player-info noftaly'];
-		this.regex = /(?:player|joueur)-?info(?:rmation)?s?/gmui;
-	}
+  constructor() {
+    super('playerinfo');
+    this.usage = `${config.bot.prefix}player-info <pseudo>`;
+    this.examples.push('player-info noftaly');
+    this.regex = /(?:player|joueur)-?info(?:rmation)?s?/gmui;
+  }
 
-	async execute(message, args){
-		let msg = await message.channel.send("Je vais chercher ça... (1/3)");
+  async execute(message, args) {
+    const msg = await message.channel.send('Je vais chercher ça... (1/2)');
 
-		const options = {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify(args)
-		};
-		// On récupère l'UUID du joueur a partir de son pseudo
-		const data_uuidFromName = await fetch(`${config.miscellaneous.api_mojang_api}/profiles/minecraft`, options)
-			.then(async response => {
-				if (response.status !== 200) return httpError(response, message);
-				return await response.json();
-			}).catch(err => console.error(err));
+    // On récupère l'UUID du joueur a partir de son pseudo
+    const uuid = await axios(`${config.apis.mojang_api}/users/profiles/minecraft/${args[0]}`, { method: 'GET' })
+      .then(async (response) => {
+        if (response.status === 204) { discordError('Impossible de trouver ce joueur, désolé', message); return -1; }
+        if (response.status !== 200) { this.httpError(response, message); return -1; }
+        return response.data.id;
+      }).catch(err => console.error(err));
 
-		if (!data_uuidFromName) return discordError("Une erreure est survenue, désolé.", message);
-		if (data_uuidFromName.length === 0) {
-			msg.delete();
-			return discordError(this.config.pseudoNotFound, message);
-		}
-		
-		// On extrait l'UUID de la réponse obtenue
-		const uuid = data_uuidFromName[0].id;
+    if (uuid === -1) return;
+    if (!uuid) return discordError('Une erreur est survenue, désolé.', message);
 
-		msg.edit("Je vais chercher ça... (2/3)");
-		// On récupère l'historique des pseudos du joueur
-		const data_nameHystory = await fetch(`${config.miscellaneous.api_mojang_api}/user/profiles/${uuid}/names`, { method: 'GET' }
-			).then(async response => {
-				if (response.status !== 200) return httpError(response, message);
-				return await response.json();
-			}).catch(err => console.error(err));
+    msg.edit('Je vais chercher ça... (2/2)');
+    // On récupère l'historique des pseudos du joueur
+    const nameHystory = await axios(`${config.apis.mojang_api}/user/profiles/${uuid}/names`, { method: 'GET' })
+      .then(async (response) => {
+        if (response.status !== 200) return this.httpError(response, message);
+        return response.data;
+      }).catch(err => console.error(err));
 
-		if (!data_nameHystory) return discordError("Une erreure est survenue, désolé.", message);
-		
-		msg.edit("Je vais chercher ça... (3/3)");
-		const headBuffer = await fetch(`http://cravatar.eu/helmavatar/${uuid}/128.png`, { method: 'GET' }
-			).then(response => {
-				if (response.status !== 200) return httpError(response, message);
-				return response.buffer();
-			}).catch(err => console.error(err));
+    if (!nameHystory) return discordError('Une erreur est survenue, désolé.', message);
 
-		if (!headBuffer) return discordError("Une erreure est survenue, désolé.", message);
-		msg.delete();
-		this.sendDetails(message, data_nameHystory, headBuffer, uuid);
-	}
+    msg.delete();
+    this.sendDetails(message, nameHystory, uuid);
+  }
 
-	httpError(response, message) {
-		console.error(`[HTTP request failed] Error : ${response.status}`);
-		discordError(`Une erreure est survenue lors de la reqûete... Veuillez réessayer plus tard.\nStatus de la requête : ${response.status} ${response.status === 429 ? "Trop de requêtes ! Attendez un peu..." : ""}`, message)
-	}
-	
-	sendDetails(message, data, img, uuid) {
-		const embed = new RichEmbed()
-			.setColor(config.bot.color)
-			.setAuthor(`Informations sur l'UUID ${uuid}`, config.bot.avatar)
-			.setFooter(`Executé par ${message.author.username} | Données fournies par https://api.mojang.com/`)
-			.setTimestamp()
-			.addField(this.config.embed.pseudo, `\`${data[data.length - 1].name}\``, false);
+  sendDetails(message, data, uuid) {
+    const embed = new RichEmbed()
+      .setColor(config.colors.default)
+      .setAuthor(`Informations sur l'UUID ${uuid}`, config.bot.avatar)
+      .setFooter(`Executé par ${message.author.username} | Données fournies par https://api.mojang.com/ et http://cravatar.eu/`)
+      .setTimestamp()
+      .addField(this.config.embed.pseudo, `\`${data[data.length - 1].name}\``, false);
 
-		if (img) {
-			embed.attachFile({ attachment: img, name: "image.png" })
-				.setThumbnail("attachment://image.png");
-		}
-	
-		let history = '';
-		if (data.length === 1) {
-			history = this.config.noHistory;
-		} else {
-			let i = 1;
-			for (let pseudo of data) {
-				history += `${i} - \`${pseudo.name}\``
-				if (pseudo.changedToAt)
-					history += `(changé ${formatDate(pseudo.changedToAt)})`;
-	
-				history += `\n`;
-				i++;
-			}
-		}
-		embed.addField(this.config.embed.history, history, false);
-		message.channel.send(embed);
-	}
+    embed.setThumbnail(`${config.apis.skin}${uuid}/128`);
 
-};
+    let history = '';
+    if (data.length === 1) {
+      history = this.config.noHistory;
+    } else {
+      let i = 1;
+      for (const pseudo of data) {
+        history += `${i} - \`${pseudo.name}\``;
+        if (pseudo.changedToAt) history += `(changé ${formatDate(pseudo.changedToAt)})`;
+
+        history += '\n';
+        i += 1;
+      }
+    }
+    embed.addField(this.config.embed.history, history, false);
+    message.channel.send(embed);
+  }
+
+  httpError(response, message) {
+    console.error(`[HTTP request failed] Error : ${response.status}`);
+    discordError(`Une erreur est survenue lors de la reqûete... Veuillez réessayer plus tard.\nStatus de la requête : ${response.status} ${response.status === 429 ? 'Trop de requêtes ! Attendez un peu...' : ''}`, message)
+  }
+}
 
 export default PlayerInfos;
