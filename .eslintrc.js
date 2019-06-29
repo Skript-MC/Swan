02a3fcfa29466b9c67f4f5eb7156aa69e8cc36b4
noftaly <34779161+noftaly@users.noreply.github.com>
2019-06-29 13:33:00 +0200

    disabled silly rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,13 @@
-module.exports = {
-    "extends": "airbnb-base"
+export default {
+    "extends": "airbnb-base",
+    "rules": {
+        "no-console": "off",
+        "no-plusplus": "off",
+        "no-continue": "off",
+        "no-else-return": "off",
+        "no-await-in-loop": "off",
+        "no-restricted-syntax": "off",
+        "class-methods-use-this": "off",
+        "no-restricted-globals": "off",
+    }
 };
