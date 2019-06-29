a3449063eb44d8ccda6355c025f2c7f820259615
noftaly <34779161+noftaly@users.noreply.github.com>
2019-06-29 13:34:52 +0200

    Add files via upload

--- src/setup.js
+++ src/setup.js
@@ -1,97 +1,113 @@
+/* eslint-disable no-await-in-loop */
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable import/no-cycle */
 import fs from 'fs';
 import axios from 'axios';
 import Discord from 'discord.js';
 import { success } from './components/Messages';
 import { config, commands } from './main';
+
 require('dotenv').config();
 
 const apikeys = {
-    discord: process.env.DISCORD_API,
-	skripthub: process.env.SKRIPTHUB_API
+  discord: process.env.DISCORD_API,
+  skripthub: process.env.SKRIPTHUB_API,
+};
+
+const GET = { method: 'GET' };
+const GETtoken = {
+  method: 'GET',
+  headers: {
+    Authorization: `Token ${apikeys.skripthub}`,
+  },
 };
 
 export function loadBot() {
-	const client = new Discord.Client()
-	client.login(apikeys.discord);
-	return client;
+  const client = new Discord.Client();
+  client.login(apikeys.discord);
+  return client;
 }
 
 
 export async function loadCommands(path) {
-	if (!path) path = 'commands';
-	console.log(`loading : ${path}`);
-	fs.readdir(`${__dirname}/${path}`, (err, files) => {
-		if (err) throw err;
-		for (let file of files) {
-			const stat = fs.statSync(`${__dirname}/${path}/${file}`);
-			if (stat.isDirectory()) {
-				loadCommands(`${path}/${file}`);
-			} else if (file === ".DS_Store") {
-				continue;
-			} else {
-				try {
-					const module = require(`${__dirname}/${path}/${file}`).default;
-					const command = new module();
-					command.init();
-					command.category = path.replace('commands/', '');
-					commands.push(command);
-				} catch (e) {
-					console.error(`Unable to load this command: ${file}`);
-					console.error(e);
-				}
-			}
-		}
-	})
+  if (!path) path = 'commands';
+  console.log(`loading : ${path}`);
+  fs.readdir(`${__dirname}/${path}`, (err, files) => {
+    if (err) throw err;
+    for (const file of files) {
+      const stat = fs.statSync(`${__dirname}/${path}/${file}`);
+      if (stat.isDirectory()) {
+        loadCommands(`${path}/${file}`);
+      } else if (file === '.DS_Store') {
+        continue;
+      } else {
+        try {
+          const Module = require(`${__dirname}/${path}/${file}`).default;
+          const command = new Module();
+          command.init();
+          command.category = path.replace('commands/', '');
+          commands.push(command);
+        } catch (e) {
+          console.error(`Unable to load this command: ${file}`);
+          console.error(e);
+        }
+      }
+    }
+  });
 }
 
 export async function loadSkriptHubAPI() {
-	const options = {
-		method: 'GET',
-		headers: {
-			'Authorization': `Token ${apikeys.skripthub}`
-		}
-	}
+  const syntaxes = [];
+  await axios(`${config.apis.syntax}/syntax/`, GETtoken)
+    .then((response) => {
+      for (const syntax of response.data) {
+        syntaxes[syntax.id] = syntax;
+      }
+    }).catch(err => console.error(err.message));
 
-	let syntaxes = [];
-	await axios(`${config.miscellaneous.api_syntax}/syntax/`, options)
-		.then(response => {
-			for (let syntax of response.data) {
-				syntaxes[syntax.id] = syntax;
-			}
-		}).catch(err => console.error(err.message));
+  await axios(`${config.apis.syntax}/syntaxexample/`, GETtoken)
+    .then((response) => {
+      for (const example of response.data) {
+        if (syntaxes[example.syntax_element]) {
+          syntaxes[example.syntax_element].example = example;
+        }
+      }
+    }).catch(err => console.error(err.message));
 
-	await axios(`${config.miscellaneous.api_syntax}/syntaxexample/`, options)
-		.then(response => {
-			for (let example of response.data) {
-				if (syntaxes[example.syntax_element]) {
-					syntaxes[example.syntax_element].example = example;
-				}
-			}
-		}).catch(err => console.error(err.message));
-
-	success('SkriptHub\'s api loaded!');
-	return syntaxes;
+  success('SkriptHub\'s api loaded!');
+  return syntaxes;
 }
 
-export async function loadSkripttoolsAPI() {
-	const options = { method: 'GET' };
-	const addons = [];
+export async function loadSkripttoolsAddons() {
+  const addons = [];
 
-	const allAddons = await axios(config.miscellaneous.api_addons, options)
-		.then(response => {
-			return response.data.data;
-		}).catch(err => console.error(err.message));
+  const allAddons = await axios(config.apis.addons, GET)
+    .then(response => response.data.data)
+    .catch(err => console.error(err.message));
 
-	for (let addon of Object.keys(allAddons)) {
-		const versions = allAddons[addon];
-		const latest = versions[versions.length - 1];
-		addon = await axios(`${config.miscellaneous.api_addons}${latest}`, options)
-			.then(response => {
-				return response.data.data;
-			}).catch(err => console.error(err.message));
-		addons.push(addon);
-	}
+  for (let addon of Object.keys(allAddons)) {
+    const versions = allAddons[addon];
+    const latest = versions[versions.length - 1];
+    addon = await axios(`${config.apis.addons}${latest}`, GET)
+      .then(response => response.data.data)
+      .catch(err => console.error(err.message));
+    addons.push(addon);
+  }
+  success('Skripttools\'s addons loaded!');
+  return addons;
+}
 
-	success('Skripttools\'s api loaded!');
-	return addons;
+export async function loadSkripttoolsSkript() {
+  const data = await axios(config.apis.skript, GET)
+    .then(response => response.data)
+    .catch(err => console.error(err));
+
+  const latest = data.data[data.data.length - 1].replace(/\s/gimu, '+');
+
+  const infos = await axios(`${config.apis.skript}${latest}`, GET)
+    .then(response => response.data)
+    .catch(err => console.error(err));
+
+  success('Skripttools\'s skript infos loaded!');
+  return infos;
 }
