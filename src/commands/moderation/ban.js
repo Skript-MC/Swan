37ff1bd863126774e1250160c82c9f1633653eb2
noftaly <34779161+noftaly@users.noreply.github.com>
2019-06-30 23:15:44 +0200

    Add files via upload

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -1,8 +1,8 @@
 import Command from '../../components/Command';
-import { modLog } from '../../components/Log';
+import { modLog } from '../../components/Moderation';
 import { discordError, discordSuccess } from '../../components/Messages';
 import { config, database } from '../../main';
-import { secondToDate } from '../../utils';
+import { secondToDuration } from '../../utils';
 
 const durations = {
   '(\\d+)s(econde?s?)?': 1,
@@ -28,8 +28,8 @@ class Ban extends Command {
     if (!victim) return discordError(this.config.missingUserArgument, message);
     if (!args[1]) return discordError(this.config.missingTimeArgument, message);
     if (!args[2]) return discordError(this.config.missingReasonArgument, message);
-    if (victim.id === message.author.id) return discordError(this.config.unableToSelfBan, message);
-    if (victim.highestRole.position >= message.member.highestRole.position) return discordError(this.config.userTooPowerful, message);
+    // if (victim.id === message.author.id) return discordError(this.config.unableToSelfBan, message);
+    // if (victim.highestRole.position >= message.member.highestRole.position) return discordError(this.config.userTooPowerful, message);
     // Regarde dans la database si le joueur est ban :
     database.find({ member: victim.id, sanction: 'ban' }, async (err, results) => {
       if (err) console.error(err);
@@ -51,7 +51,7 @@ class Ban extends Command {
       let chan = message.guild.channels.find(c => c.name === `${config.moderation.banChannelPrefix}${victim.user.username.replace(/[^a-zA-Z]/gimu, '').toLowerCase()}` && c.type === 'text');
       if (!chan) {
         try {
-          chan = await message.guild.createChannel(`${config.moderation.banChannelPrefix}${victim.user.username.replace(/[^a-zA-Z]/gimu, '').toLowerCase()}`, 'text');
+          chan = await message.guild.createChannel(`${config.moderation.banChannelPrefix}${victim.user.username.replace(/[^a-zA-Z0-9]/gimu, '').toLowerCase()}`, { type: 'text' });
           chan.setParent(config.moderation.log.categoryID);
           await chan.overwritePermissions(message.guild.roles.find(r => r.name === '@everyone'), { VIEW_CHANNEL: false });
           await chan.overwritePermissions(message.guild.roles.find(r => r.name === 'Staff'), { VIEW_CHANNEL: true });
@@ -72,7 +72,7 @@ class Ban extends Command {
       const success = this.config.successfullyBanned
         .replace('%u', `${victim.user.username}`)
         .replace('%r', reason)
-        .replace('%d', secondToDate(duration));
+        .replace('%d', secondToDuration(duration));
 
       discordSuccess(success, message);
       chan.send(this.config.whyHere.replace('%u', `${victim.user.username}`));
