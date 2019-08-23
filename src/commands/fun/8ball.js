c3178c7be6f3f359f421e273051d67877a198aa3
noftaly <34779161+noftaly@users.noreply.github.com>
2019-08-23 19:29:43 +0200

    8ball.js now requires a question

--- src/commands/fun/8ball.js
+++ src/commands/fun/8ball.js
@@ -1,5 +1,6 @@
 import Command from '../../components/Command';
 import { config } from '../../main';
+import { discordError } from '../../components/Messages';
 
 const affirmative = ['Oui.', 'Oui !', 'D\'après moi, oui !', 'Je le pense', 'C\'est une chose sûre !', 'C\'est certain.', 'Sans aucun doute', 'Il me semble...', 'Pourquoi demander ? La réponse parait évidente ! Oui !', 'ET C\'EST UN OUI !', 'Oui ! NAN J\'AI PERDU :sob: Je suis vraiment nul au ni-oui ni-non :weary: ', 'Affirmatif, chef.', 'Positif.'];
 const negative = ['Non.', 'MDR NON', 'C\'est un non.', 'Mes sources me confirment que non.', 'C\'est mieux que tu ne sois pas au courant...', 'Bien sur que non !', 'Je ne suis pas sur de comprendre... Dans le doute je vais dire non.', 'Question très compliquée... Mais je dirai non.', 'Ou... Non ! C\'est Non !', 'Bien sur que non...', 'Négatif', 'Je répondrai par la négation. Rah chui trop fort à ni-oui ni-non ! Tu me battras jamais :wink:', 'Sûrement pas.'];
@@ -7,12 +8,13 @@ const negative = ['Non.', 'MDR NON', 'C\'est un non.', 'Mes sources me confirmen
 class EightBall extends Command {
   constructor() {
     super('8ball');
+    this.regex = /(?:8|eight)-?ball/gimu;
     this.usage = `${config.bot.prefix}8ball <question>`;
     this.examples.push('8ball suis-je le plus beau ?');
-    this.regex = /(?:8|eight)-?ball/gmui;
   }
 
-  async execute(message, _args) {
+  async execute(message, args) {
+    if (args.length < 1) return discordError(this.config.noQuestion, message);
     let answer;
     if (Math.random() < 0.5) {
       answer = affirmative[Math.floor(Math.random() * affirmative.length)];
