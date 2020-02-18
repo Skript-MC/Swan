67e3c5be909a664243742aee285e92ad7fb80f67
Elliot Maisl <noftaly@gmail.com>
2020-02-18 22:57:42 +0100

    Create bassboost.js

---
+++ src/commands/music/bassboost.js
@@ -0,0 +1,34 @@
+import Command from '../../helpers/Command';
+import MusicBot from '../../helpers/music';
+import { config } from '../../main';
+
+class Bassboost extends Command {
+  constructor() {
+    super('Bassboost');
+    this.aliases = ['bassboost'];
+    this.usage = 'bassboost [<nombre entre 0 et 40>]';
+    this.examples = ['bassboost', 'bassboost 3'];
+    this.enabledInHelpChannels = false;
+  }
+
+  async execute(message, args) {
+    const validate = MusicBot.canUseCommand(message, { songPlaying: true });
+    if (validate !== true) return message.channel.send(config.messages.errors.music[validate]);
+
+    if (args.length === 0) return message.channel.send(this.config.currentPower.replace('%d', MusicBot.bassboost));
+
+    let power = parseInt(args.join(' '), 10);
+    if (['off', 'default', 'stop'].includes(args[0])) power = 0;
+
+    if (isNaN(power) || power < 0 || power > 40) return message.channel.send(this.config.invalidPower);
+
+    MusicBot.askPermission(this.bassboost, 'changer la puissance des basses', message, power, this.config);
+  }
+
+  bassboost(message, arg, cmdConfig) {
+    MusicBot.bassboost = arg;
+    message.channel.send(cmdConfig.changed.replace('%d', arg));
+  }
+}
+
+export default Bassboost;
