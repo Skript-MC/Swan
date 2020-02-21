893cb3d62a2d3c0cbcc794b39080346d5f1fa3d4
Elliot Maisl <noftaly@gmail.com>
2020-02-21 19:02:47 +0100

    Create messageDelete.js

---
+++ src/events/messageDelete.js
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-cycle */
+import { config } from '../main';
+
+export default async function messageDeleteHandler(message) {
+  if (message.member.roles.highest.position >= message.guild.roles.cache.get(config.roles.staff).position) return;
+  if (message.mentions.users.size === 0 && message.mentions.roles.size === 0) return;
+
+  const mentions = [
+    ...message.mentions.users.array().map(elt => elt.username),
+    ...message.mentions.roles.array().map(elt => elt.name),
+  ];
+  message.channel.send(
+    config.messages.miscellaneous.ghostping
+      .replace('%s', mentions.join(', '))
+      .replace('%m', message.member.nickname || message.author.username),
+  );
+}
