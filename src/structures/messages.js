fdbbed186ca13461cf33721e36d4f7a6ea676a54
noftaly <noftaly@gmail.com>
2020-02-24 18:47:31 +0100

    🎨 Show nickname instead of username in message embeds

--- src/structures/messages.js
+++ src/structures/messages.js
@@ -14,39 +14,39 @@ export function error(msg) {
 
 export function discordSuccess(description, message) {
   const embed = new MessageEmbed()
-    .setAuthor(message.author.username, message.author.avatarURL)
+    .setAuthor(message.member.nickname || message.author.username, message.author.avatarURL)
     .attachFiles(['./assets/success.png'])
     .setThumbnail('attachment://success.png')
     .setTitle('Succès')
     .setColor(config.colors.success)
     .setDescription(description)
     .setTimestamp()
-    .setFooter(`Exécuté par ${message.author.username}`);
+    .setFooter(`Exécuté par ${message.member.nickname || message.author.username}`);
   return embed;
 }
 
 export function discordInfo(description, message) {
   const embed = new MessageEmbed()
-    .setAuthor(message.author.username, message.author.avatarURL)
+    .setAuthor(message.member.nickname || message.author.username, message.author.avatarURL)
     .attachFiles(['./assets/information.png'])
     .setThumbnail('attachment://information.png')
     .setTitle('Information')
     .setColor(config.colors.default)
     .setDescription(description)
     .setTimestamp()
-    .setFooter(`Exécuté par ${message.author.username}`);
+    .setFooter(`Exécuté par ${message.member.nickname || message.author.username}`);
   return embed;
 }
 
 export function discordError(description, message) {
   const embed = new MessageEmbed()
-    .setAuthor(message.author.username, message.author.avatarURL)
+    .setAuthor(message.member.nickname || message.author.username, message.author.avatarURL)
     .attachFiles(['./assets/error.png'])
     .setThumbnail('attachment://error.png')
     .setTitle('Erreur')
     .setColor(config.colors.error)
     .setDescription(description)
     .setTimestamp()
-    .setFooter(`Exécuté par ${message.author.username}`);
+    .setFooter(`Exécuté par ${message.member.nickname || message.author.username}`);
   return embed;
 }
