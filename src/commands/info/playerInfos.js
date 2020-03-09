37112bbccc500468e61cc99e0c4cb7620366b6e6
Romitou <romain76.leprevost@gmail.com>
2020-03-09 20:51:22 +0100

    💬 PlayerInfo.js messages --> config.json

--- src/commands/info/playerInfos.js
+++ src/commands/info/playerInfos.js
@@ -15,20 +15,20 @@ class PlayerInfos extends Command {
 
   async execute(message, args) {
     if (args.length === 0) return message.channel.send(discordError(this.config.invalidCmd, message));
-    const msg = await message.channel.send('Je vais chercher ça... (1/2)');
+    const msg = await message.channel.send(this.config.searching1);
 
     // On récupère l'UUID du joueur a partir de son pseudo
     const uuid = await axios(`${config.apis.mojang_api}/users/profiles/minecraft/${args[0]}`)
       .then(async (response) => {
-        if ([204, 400, 404].includes(response.status)) { message.channel.send(discordError('Impossible de trouver ce joueur, désolé.', message)); return -1; }
+        if ([204, 400, 404].includes(response.status)) { message.channel.send(discordError(this.config.playerNotFound, message)); return -1; }
         if (response.status !== 200) { this.httpError(response, message); return -1; }
         return response.data.id;
       }).catch(console.error);
 
     if (uuid === -1) return;
-    if (!uuid) return message.channel.send(discordError('Une erreur est survenue, désolé.', message));
+    if (!uuid) return message.channel.send(discordError(this.config.error, message));
 
-    msg.edit('Je vais chercher ça... (2/2)');
+    msg.edit(this.config.searching2);
     // On récupère l'historique des pseudos du joueur
     const nameHystory = await axios(`${config.apis.mojang_api}/user/profiles/${uuid}/names`)
       .then(async (response) => {
@@ -36,7 +36,7 @@ class PlayerInfos extends Command {
         return response.data;
       }).catch(console.error);
 
-    if (!nameHystory) return message.channel.send(discordError('Une erreur est survenue, désolé.', message));
+    if (!nameHystory) return message.channel.send(discordError(this.config.error, message));
 
     msg.delete();
     this.sendDetails(message, nameHystory, uuid);
