76aed54e4bbcc072e9beffbd314945308c477371
Romain <admin@romitou.fr>
2020-03-23 19:21:22 +0100

    🎨 Fix developers and contributors

--- src/commands/basic/statistics.js
+++ src/commands/basic/statistics.js
@@ -14,7 +14,7 @@ class Statistics extends Command {
   }
 
   async execute(message, _args) {
-    const uptime = secondToDuration(client.uptime / 1000);
+    const uptime = secondToDuration(client.uptime /	 1000);
 
     const onlineUsers = message.guild.members.cache.filter(m => (m.presence.status === 'online' || m.presence.status === 'idle' || m.presence.status === 'dnd') && !m.user.bot).size;
     const totalUsers = message.guild.members.cache.filter(m => !m.user.bot).size;
@@ -25,16 +25,17 @@ class Statistics extends Command {
     const embed = new MessageEmbed()
       .setColor(config.colors.default)
       .attachFiles([config.bot.avatar])
-      .setAuthor('Statistiques sur le bot', 'attachment://logo.png')
-      .addField('Préfix', config.bot.prefix, true)
+      .setAuthor('Statistiques de Swan', 'attachment://logo.png')
+      .addField('Préfixe', config.bot.prefix, true)
       .addField('Version', pkg.version, true)
       .addField('Temps de fonctionnement', uptime, true)
       .addField('Mémoire', `${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)} MB`, true)
       .addField('Commandes', commands.length, true)
       .addField('Répartition des membres', `${onlineUsers} en ligne / ${offlineUsers} hors ligne / ${totalBots} bot${totalBots > 1 ? 's' : ''}`, true)
       .addField('Total', `${total} membres`, true)
-      .addField('Développeurs', `${pkg.authors.join(', ')}. Merci aussi aux contributeurs (Romitou, iTrooz_) !`, true)
-      .addField('Signalement des bugs/problèmes, et suggestions', `${pkg.bugs.url}`, true)
+      .addField('Développeurs', `${pkg.authors.join('\n')}`, true)
+      .addField('Contributeurs', `${pkg.contributors.join('\n')}`, true)
+      .addField('Signalement des bugs/problèmes, et suggestions', `${pkg.bugs.url} ou sur le [Discord de Swan](https://discord.gg/njSgX3w)`, true)
       .setFooter(`Exécuté par ${message.author.username}`)
       .setTimestamp();
     message.channel.send(embed);
