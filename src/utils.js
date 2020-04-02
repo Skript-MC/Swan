9b998af204be628d237a3562ae8a58024a1d0b9e
Romitou <romain76.leprevost@gmail.com>
2020-04-02 20:45:08 +0200

    ✨ Add auto-change pseudo

--- src/utils.js
+++ src/utils.js
@@ -109,6 +109,18 @@ export function prunePseudo(member) {
 }
 
 /**
+ * @description Élaguer le pseudo d'un membre, pour qu'il puisse être mentionné par tout le monde.
+ * @param {GuildMember} member Le membre
+ */
+export function prunePseudoJoin(member) {
+  const name = member.nickname || member.user.username;
+  // Si le pseudonyme de ce joueur ne correspond pas au regex imposé, alors on retourne true.
+  if (name.match(new RegExp(/[^a-zA-Z0-9-ÖØ-öø-ÿ]/gimu))) return true;
+  // Sinon, son pseudo est correct et on retourne false. Il n'a pas besoin d'être modifié.
+  return false;
+}
+
+/**
  * @description Fonction pour calculer la distance jaro-winkler entre 2 strings
  * @param {String} s1 Premier string
  * @param {String} s2 Second string
