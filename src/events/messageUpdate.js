db12c838e10dcaba715946804dc5a475a5ed26cb
noftaly <noftaly@gmail.com>
2020-04-04 18:29:28 +0200

    ✨ Improve anti-ghostping & update purge description

--- src/events/messageUpdate.js
+++ src/events/messageUpdate.js
@@ -3,18 +3,19 @@ import { config } from '../main';
 
 export default async function messageUpdateHandler(oldMessage, newMessage) {
   if (newMessage.author.bot) return; // The bot has a ghost-ping command
-  // if (newMessage.member.roles.highest.position >= newMessage.guild.roles.cache.get(config.roles.staff).position) return;
-  if (oldMessage.mentions.users.size === 0 && oldMessage.mentions.roles.size === 0) return;
+  if (newMessage.member.roles.highest.position >= newMessage.guild.roles.cache.get(config.roles.staff).position) return;
 
-  const oldUserMentions = oldMessage.mentions.users.array().map(elt => elt.username);
-  const oldRoleMentions = oldMessage.mentions.roles.array().map(elt => elt.name);
+  const oldUserMentions = oldMessage.mentions.users.array().filter(usr => !usr.bot).map(usr => usr.username);
+  const oldRoleMentions = oldMessage.mentions.roles.array().map(role => role.name);
   const oldMentions = [...oldUserMentions, ...oldRoleMentions];
 
-  const newUserMentions = newMessage.mentions.users.array().map(elt => elt.username);
-  const newRoleMentions = newMessage.mentions.roles.array().map(elt => elt.name);
+  const newUserMentions = newMessage.mentions.users.array().map(usr => usr.username);
+  const newRoleMentions = newMessage.mentions.roles.array().map(role => role.name);
   const newMentions = [...newUserMentions, ...newRoleMentions];
 
   const deletedMentions = oldMentions.filter(elt => !newMentions.includes(elt));
+  if (deletedMentions.length === 0) return;
+
   // FIXME: Make this work (it does not work due to a current discordjs bug)
   const deletedRoleMention = oldRoleMentions.filter(elt => !newRoleMentions.includes(elt)).length > 0;
 
