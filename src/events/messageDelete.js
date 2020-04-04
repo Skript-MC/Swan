ca809f650551cabba1026665fe4a31765766ef4f
noftaly <noftaly@gmail.com>
2020-04-04 18:29:28 +0200

    ✨ Improve anti-ghostping & update purge description

--- src/events/messageDelete.js
+++ src/events/messageDelete.js
@@ -3,12 +3,13 @@ import { config } from '../main';
 
 export default async function messageDeleteHandler(message) {
   if (message.author.bot) return; // The bot has a ghost-ping command
-  // if (message.member.roles.highest.position >= message.guild.roles.cache.get(config.roles.staff).position) return;
-  if (message.mentions.users.size === 0 && message.mentions.roles.size === 0) return;
+  if (message.member.roles.highest.position >= message.guild.roles.cache.get(config.roles.staff).position) return;
 
-  const userMentions = message.mentions.users.array().map(elt => elt.username);
-  const roleMentions = message.mentions.roles.array().map(elt => elt.name);
+  const userMentions = message.mentions.users.array().filter(usr => !usr.bot).map(usr => usr.username);
+  const roleMentions = message.mentions.roles.array().map(role => role.name);
   const mentions = [...userMentions, ...roleMentions];
+
+  if (userMentions.length === 0 && roleMentions.length === 0) return;
 
   const deletedRoleMention = roleMentions.length > 0;
 
