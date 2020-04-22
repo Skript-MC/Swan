8218c7b3436740f777cada49eb3ee2a8ecd8bbdd
Romitou <admin@romitou.fr>
2020-04-22 14:47:42 +0200

    🐛 Relaxation of testing regex - Fix guildMemberAdd event - Fix message in config to inform the user - 1/2 of name must be mentionned

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,10 +1,10 @@
 /* eslint-disable import/no-cycle */
-import { prunePseudoJoin } from '../utils';
+import { regexPseudo } from '../utils';
 import { config } from '../main';
 
 export default async function memberAddHandler(member) {
   // On vérifie si le pseudonyme correspond aux critères.
-  if (prunePseudoJoin(member) === true) {
+  if (regexPseudo(member) === true) {
     // On en choisit un futur pseudonyme au hazard dans la liste.
     const name = config.messages.miscellaneous.renameList[Math.floor(Math.random() * config.messages.miscellaneous.renameList.length)];
     // On modifie son pseudonyme et on lui envoie un message expliquant pourquoi.
