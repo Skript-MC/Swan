4c06ca0bc023605345eceb3b5db56df8c14b21c5
Romitou <romitou@protonmail.com>
2020-05-03 21:53:28 +0200

    ✨ New toValidName function Co-authored by noftaly

--- src/utils.js
+++ src/utils.js
@@ -173,6 +173,20 @@ export function jkDistance(s1, s2) {
   return weight;
 }
 
+export function toValidName(str) {
+  const valid = [];
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (['é', 'è', 'à', 'ù', 'ô', 'â', 'ê', 'ë', 'î', 'ï'].includes(char.toLowerCase())) {
+      valid.push(char);
+      continue;
+    }
+    const charcode = str.charCodeAt(i);
+    if (charcode < 0x80) valid.push(char);
+  }
+  return valid.join('');
+}
+
 export function slugify(string) {
   const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;';
   const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnooooooooprrsssssttuuuuuuuuuwxyyzzz------';
