b2cacd46947f7a058ea6ee09cbe7eba7bb3397aa
Romitou <romitou@protonmail.com>
2020-05-04 22:25:47 +0200

    💩 Rework style (needs to be improved)

--- scripts/docs.js
+++ scripts/docs.js
@@ -1,18 +1,14 @@
 /* eslint-disable import/no-cycle */
 import fs from 'fs';
 
-const template = `
-#### $name\n
+const template = `\n
+### <a name="$name"></a>$name\n
 - **Catégorie :** $category
 - **Description :** $description
-- **Aliases :** \`$aliases\`
+- **Alias :** \`$aliases\`
 - **Usage :** \`$usage\`
 - **Exemples :** \`$examples\`
-- **Cooldown :** $cooldown
-- **Informations supplémentaires :**
-  - Activée dans les canaux d'aide : $enabledInHelpChannels
-  - Permission(s) nécéssaire(s) pour éxécuter la commande : $permissions
-  - Canaux requis / interdits : $requiredChannels / $prohibitedChannels
+- **Délai de réutilisation :** $cooldown
 `;
 
 async function softLoadCommands(path) {
@@ -67,29 +63,33 @@ async function getAllCommands() {
 
 async function generateDocs() {
   const commands = await getAllCommands();
-  let header = `# Documentation\n\nDocumentation de toutes les commandes de Swan (${commands.length})\n\n## Index\n\n`;
+  let header = `# Documentation de Swan\n\nCette page regroupe les **${commands.length} commandes** disponibles sur Swan.\n\n## Accès rapides\n\n`;
   let content = '\n## Commandes\n';
 
   const cmds = commands.sort((a, b) => a.name.localeCompare(b.name));
   for (const cmd of cmds) {
     header += `- [${cmd.name}]`; // Index name
-    header += `(#### ${cmd.name})\n`.replace(/ +/g, '-'); // Index link
+    header += `(#${cmd.name})\n`.replace(/ +/g, '-'); // Index link
 
     content += template
       .replace('$name', cmd.name)
+      .replace('$name', cmd.name)
       .replace('$category', cmd.category)
       .replace('$description', cmd.description)
-      .replace('$aliases', cmd.aliases.join('` | `'))
+      .replace('$aliases', cmd.aliases.join('`, `'))
       .replace('$usage', cmd.usage)
-      .replace('$examples', cmd.examples.join('` | `'))
-      .replace('$cooldown', cmd.cooldown === 0 ? 'aucun' : `${cmd.cooldown / 1000} secondes`)
-      .replace('$enabledInHelpChannels', cmd.enabledInHelpChannels ? '✅' : '❌')
-      .replace('$permissions', cmd.permissions.join(', ') || 'aucune')
-      .replace('$requiredChannels', cmd.requiredChannels.length || 'aucun')
-      .replace('$prohibitedChannels', cmd.prohibitedChannels.length || 'aucun');
+      .replace('$examples', cmd.examples.join('`, `'))
+      .replace('$cooldown', cmd.cooldown === 0 ? 'aucun' : `${cmd.cooldown / 1000} secondes`);
+    if (cmd.permissions.join(', ') || cmd.requiredChannels.length || cmd.prohibitedChannels.length || !cmd.enabledInHelpChannels) {
+      content += '- **Informations supplémentaires :**\n';
+      if (!cmd.enabledInHelpChannels) content += '\t- ⚠️ Cette commande ne peut pas être exécutée dans les salons d\'aide.\n';
+      if (cmd.permissions.join(', ')) content += `\t- ⚠️ Seul les membres ayant un rôle **${cmd.permissions.join(', ')}** peuvent exécuter cette commande.\n`;
+      if (cmd.requiredChannels.length) content += `\t- ⚠️ Les canaux requis sont **${cmd.requiredChannels.length}**.\n`;
+      if (cmd.prohibitedChannels.length) content += `\t- ⚠️ Cette commande est interdite dans les salons **${cmd.prohibitedChannels.length}**.\n`;
+    }
   }
 
-  const path = `${__dirname}/../docs/commands.md`;
+  const path = `${__dirname}/../docs/index.md`;
   try {
     fs.writeFile(path, header + content, (err) => {
       if (err) throw new Error(err);
