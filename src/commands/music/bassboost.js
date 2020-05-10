1ff8345e56b561e82f2647125988d7a128a2fbd7
noftaly <noftaly@gmail.com>
2020-05-10 14:10:35 +0200

    🎨 Fix and update "play" and "bassboost" commands

--- src/commands/music/bassboost.js
+++ src/commands/music/bassboost.js
@@ -7,7 +7,7 @@ class Bassboost extends Command {
     super('Bassboost');
     this.aliases = ['bassboost'];
     this.usage = 'bassboost [<nombre entre 0 et 40>]';
-    this.examples = ['bassboost', 'bassboost 3'];
+    this.examples = ['bassboost', 'bassboost 3', 'bassboost default'];
     this.enabledInHelpChannels = false;
   }
 
@@ -18,7 +18,7 @@ class Bassboost extends Command {
     if (args.length === 0) return message.channel.send(this.config.currentPower.replace('%d', MusicBot.bassboost));
 
     let power = parseInt(args.join(' '), 10);
-    if (['off', 'default', 'stop'].includes(args[0])) power = 0;
+    if (['off', 'default', 'stop'].includes(args[0])) power = 20;
 
     if (isNaN(power) || power < 0 || power > 40) return message.channel.send(this.config.invalidPower);
 
