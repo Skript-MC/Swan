ce063d32e13bb275c5612542683cec0c8076b234
noftaly <noftaly@gmail.com>
2020-05-16 10:39:08 +0200

    ♻️ Load commands and events asynchronously And remove the init() method of Command, because it has never been used

--- src/main.js
+++ src/main.js
@@ -23,40 +23,38 @@ moment.relativeTimeThreshold('m', 55);
 moment.relativeTimeThreshold('s', 55);
 moment.relativeTimeThreshold('ss', 3);
 
-const config = loadConfig();
 
 export const client = new Client();
-client.login(process.env.DISCORD_API);
-client.config = config;
+client.config = loadConfig();
 client.logger = new Logger();
 client.activated = true;
 client.commands = [];
+client.login(process.env.DISCORD_API);
 
 loadCommands();
+loadEvents();
 
 export const db = loadDatabases();
-
 extendClasses();
-loadEvents(client);
 
-const shouldLoadSyntaxes = config.messages.commands.syntaxinfo.enabled ?? true;
-const shouldLoadAddons = config.messages.commands.addoninfo.enabled ?? true;
+const shouldLoadSyntaxes = client.config.messages.commands.syntaxinfo.enabled ?? true;
+const shouldLoadAddons = client.config.messages.commands.addoninfo.enabled ?? true;
 export const SkriptHubSyntaxes = shouldLoadSyntaxes ? loadSkriptHubAPI() : null;
 export const SkripttoolsAddons = shouldLoadAddons ? loadSkripttoolsAddons() : null;
 
 client.on('ready', async () => {
-  client.guild = client.guilds.resolve(config.bot.guild);
+  client.guild = client.guilds.resolve(client.config.bot.guild);
 
   // Verifying tokens and ids
   if (!process.env.DISCORD_API) throw new Error('Discord token was not set in the environment variables (DISCORD_API)');
   if (!process.env.YOUTUBE_API) throw new Error('Youtube token was not set in the environment variables (YOUTUBE_API)');
   if (!process.env.BOT) throw new Error('Bot id was not set in the environment variables (BOT)');
   if (!process.env.GUILD) throw new Error('Guild id was not set in the environment variables (GUILD)');
-  for (const [key, value] of Object.entries(config.channels)) {
+  for (const [key, value] of Object.entries(client.config.channels)) {
     if (!value) client.logger.warn(`config.channels.${key} is not set. You may want to fill this field to avoid any error.`);
     else if (!client.guild.channels.cache.has(value)) client.logger.warn(`The id entered for config.channels.${key} is not a valid channel.`);
   }
-  for (const [key, value] of Object.entries(config.roles)) {
+  for (const [key, value] of Object.entries(client.config.roles)) {
     if (!value) client.logger.warn(`config.roles.${key} is not set. You may want to fill this field to avoid any error.`);
     else if (!client.guild.roles.cache.has(value)) client.logger.warn(`The id entered for config.roles.${key} is not a valid role.`);
   }
@@ -78,7 +76,7 @@ client.on('ready', async () => {
   client.logger.debug('main.js -> commandsStats database initialized successfully');
 
   // Cache all messages that need to be cached
-  const suggestionChannel = client.channels.cache.get(config.channels.suggestion);
+  const suggestionChannel = client.channels.cache.get(client.config.channels.suggestion);
   const suggestionMessages = await suggestionChannel.messages.fetch({ limit: 100 }, true);
   client.logger.step(`Messages cached! (${suggestionMessages.size})`);
 
@@ -87,13 +85,13 @@ client.on('ready', async () => {
   setInterval(() => {
     Command.filterCooldown(client.commands); // Tri dans les cooldowns des commandes
     SanctionManager.checkSanctions(); // Vérification des sanctions temporaires
-  }, config.bot.checkInterval.short);
+  }, client.config.bot.checkInterval.short);
 
   setInterval(() => {
     loadRssFeed(); // Chargement des flux RSS
     loadSkriptReleases(); // Vérification si une nouvelle version de Skript est sortie
-    client.user.setPresence(randomActivity(client, client.commands, config.bot.prefix)); // On remet l'activité du bot (sinon elle s'enlève toute seule au bout d'un moment)
-  }, config.bot.checkInterval.long);
+    client.user.setPresence(randomActivity(client, client.commands, client.config.bot.prefix)); // On remet l'activité du bot (sinon elle s'enlève toute seule au bout d'un moment)
+  }, client.config.bot.checkInterval.long);
 });
 
 client.on('error', (err) => { throw new Error(err); });
