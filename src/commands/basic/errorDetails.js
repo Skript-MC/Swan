0c19185b906fc0ef7faa04e71c078c1b1d1cf558
noftaly <noftaly@gmail.com>
2020-05-23 12:56:24 +0200

    💬 Added new error-details

--- src/commands/basic/errorDetails.js
+++ src/commands/basic/errorDetails.js
@@ -24,6 +24,11 @@ class ErrorDetails extends Command {
     else if (arg.match(/There's no player in/gimu)) message.channel.send(this.config.error.noplayer);
     else if (arg.match(/code has to be put in triggers/gimu)) message.channel.send(this.config.error.codeintriggers);
     else if (arg.match(/because it's a single value/gimu)) message.channel.send(this.config.error.cantloopsinglevalue);
+    else if (arg.match(/This command doesn't have any arguments/gimu)) message.channel.send(this.config.error.doesnthaveargs);
+    else if (arg.match(/can only be used within a command/gimu)) message.channel.send(this.config.error.onlyincommand);
+    else if (arg.match(/in a periodic event/gimu)) message.channel.send(this.config.error.notinperiodic);
+    else if (arg.match(/cannot be parsed as/gimu)) message.channel.send(this.config.error.cantparse);
+    else if (arg.match(/cannot be saved/gimu)) message.channel.send(this.config.error.cantsave);
     else message.channel.sendError(this.config.error.unknown, message.member);
   }
 }
