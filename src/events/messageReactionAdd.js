6928fdf283c370b7e4f05f373d4db3f1cd750c05
noftaly <noftaly@gmail.com>
2020-06-16 11:18:57 +0200

    🐛 Fix the bot counting its own reactions for suggestions

--- src/events/messageReactionAdd.js
+++ src/events/messageReactionAdd.js
@@ -63,7 +63,7 @@ export default async function messageReactionAddHandler(reaction, user) {
       const reactionners = message.reactions.cache.find(r => r.emoji.name === '❌').users;
       if (typeof reactionners.cache.get(user.id) !== 'undefined') reactionners.remove(user);
 
-      const positive = reaction.count;
+      const positive = reaction.count - 1;
       if (positive === 20) {
         guild.channels.cache.get(client.config.channels.main).send(`:fire: La suggestion de ${message.embeds[0].title.replace(/Suggestion de (\w+) \(\d+\)/, '$1')} devient populaire ! Elle a déjà 20 avis positifs !\nAvez-vous pensé à y jeter un oeil ? Qu'en pensez-vous ?\nLien : ${link}`);
       }
@@ -71,7 +71,7 @@ export default async function messageReactionAddHandler(reaction, user) {
       const reactionners = message.reactions.cache.find(r => r.emoji.name === '✅').users;
       if (typeof reactionners.cache.get(user.id) !== 'undefined') reactionners.remove(user);
 
-      const negative = reaction.count;
+      const negative = reaction.count - 1;
       if (negative === 10) {
         guild.channels.cache.get(client.config.channels.logs).send(`:warning: La suggestion de ${message.embeds[0].title.replace(/Suggestion de (\w+) \(\d+\)/, '$1')} a reçu beaucoup de réactions négatives ! Elle a 10 avis contre.\nLien : ${link}`);
       }
