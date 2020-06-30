55aaf8f5349f2c148aea6cad613e5af10f335676
noftaly <noftaly@gmail.com>
2020-06-30 23:06:24 +0200

    🐛 Fix crashloop when banned user is hardban while the bot is off

--- src/structures/actions/UnbanAction.js
+++ src/structures/actions/UnbanAction.js
@@ -11,22 +11,25 @@ class UnbanAction extends ModerationAction {
 
   async exec(_document) {
     // Regarde dans la bdd si le joueur est banni
-    const isHardban = await db.sanctions.findOne({ member: this.data.user.id, type: ACTION_TYPE.HARDBAN }).catch(console.error);
-    if (isHardban) {
-      const isBanned = await client.guild.fetchBan(this.data.user.id).catch(() => {});
-      if (isBanned) await client.guild.members.unban(this.data.user.id, this.data.reason).catch(console.error);
-    } else {
-      const isBan = await db.sanctions.findOne({ member: this.data.user.id, type: ACTION_TYPE.BAN }).catch(console.error);
-      if (isBan) {
-        await SanctionManager.removeRole(this.data);
-        const file = await SanctionManager.removeChannel(this.data);
-        this.data.setFile(file);
-      }
+    const ban = await db.sanctions.findOne(
+      { member: this.data.victimId,
+        $or: [
+          { type: ACTION_TYPE.HARDBAN },
+          { type: ACTION_TYPE.BAN },
+        ] },
+    ).catch(console.error);
+    if (ban.type === ACTION_TYPE.HARDBAN || !this.data.member) {
+      const isBanned = await client.guild.fetchBan(this.data.victimId).catch(console.error);
+      if (isBanned) await client.guild.members.unban(this.data.victimId, this.data.reason).catch(console.error);
+    } else if (ban.type === ACTION_TYPE.BAN) {
+      await SanctionManager.removeRole(this.data);
+      const file = await SanctionManager.removeChannel(this.data);
+      this.data.setFile(file);
     }
 
     if (!this.data.sendSuccessIfBot && this.data.moderator.user.bot) return;
     const successMessage = this.config.successfullyUnbanned
-      .replace('%u', this.data.user.username)
+      .replace('%u', this.data.getUserName())
       .replace('%r', this.data.reason);
     this.data.messageChannel.sendSuccess(successMessage, this.data.moderator);
   }
