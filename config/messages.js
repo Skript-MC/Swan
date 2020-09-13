49a0c19cd9a4b792887c945d3994d08e2fb79da8
Elliot Maisl <elliot.maisl@gmail.com>
2020-09-13 11:07:08 +0200

    ✨ Send discord message for insuficient permissions

--- config/messages.js
+++ config/messages.js
@@ -1,6 +1,7 @@
 export default {
   global: {
     notAllowed: "Tu n'as pas la permission de faire cela :frowning:",
+    insufficientClientPermissions: "Je n'ai pas les permissions Discord nécessaires pour éxecuter la commande {COMMAND}. (Permissions manquantes : {PERMISSIONS})",
   },
   links: {
     embed: {
