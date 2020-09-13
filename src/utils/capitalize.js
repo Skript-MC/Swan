6f2af6351a77bfc25d5d9bcf005b879dbfb99d93
Elliot Maisl <elliot.maisl@gmail.com>
2020-09-13 21:47:36 +0200

    ✨  Add a "capitalize" utility function

---
+++ src/utils/capitalize.js
@@ -0,0 +1,3 @@
+export default function capitalize(string) {
+  return string[0].toUpperCase() + string.slice(1);
+}
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,2 @@
-// eslint-disable-next-line import/prefer-default-export
 export { default as padNumber } from './padNumber';
+export { default as capitalize } from './capitalize';
