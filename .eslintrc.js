5b13993c0491c54595613d1d32439716e124686f
Elliot Maisl <elliot.maisl@gmail.com>
2020-09-13 10:30:36 +0200

    🔧  Ignore "build" directory for ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: 'babel-eslint',
   extends: 'noftalint',
-  ignorePatterns: ['.eslintrc.js', 'node_modules/'],
+  ignorePatterns: ['.eslintrc.js', 'node_modules/', 'build/'],
   reportUnusedDisableDirectives: true,
   env: {
     node: true,
