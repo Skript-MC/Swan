6589e24d85bab775633c3f8d9026c0538102930e
Elliot Maisl <elliot.maisl@gmail.com>
2020-09-14 16:10:17 +0200

    🎨  Use Roles instead of permissions for commands

--- config/commands/moderation.js
+++ config/commands/moderation.js
@@ -2,13 +2,14 @@ import { Permissions } from 'discord.js';
 
 const permissions = Permissions.FLAGS;
 
+const hasStaffRole = message => (message.member.roles.cache.has(process.env.ROLE_STAFF) ? null : 'Pas de rôle Staff');
 
 // eslint-disable-next-line import/prefer-default-export
 export const purge = {
   settings: {
     aliases: ['purge'],
     clientPermissons: [permissions.SEND_MESSAGES, permissions.MANAGE_MESSAGES],
-    userPermissions: [permissions.MANAGE_MESSAGES],
+    userPermissions: hasStaffRole,
   },
   description: {
     content: "Permet de supprimer plusieurs messages à la fois dans un salon, avec la possibilité de spécifier un membre en particulier. Il n'est possible que de supprimer des messages ayant été envoyés il y a moins de 15 jours : c'est une limitation de discord. Par défaut, les messages des membres du staff ne seront pas inclus. Si vous voulez aussi supprimer les messages du Staff, ajoutez le drapeau `--force` (ou `-f`)",
