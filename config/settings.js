e73008ed25afd4a1fb83880fd30fb6115cf983cb
Elliot Maisl <elliot.maisl@gmail.com>
2020-09-14 11:00:48 +0200

    ✨  Add a Staff Role

--- config/settings.js
+++ config/settings.js
@@ -1,8 +1,18 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export default {
   bot: {
-    prefix: '.',
+    prefix: '=',
   },
   colors: {
     default: '#4286f4',
+  },
+  moderation: {
+    purgeLimit: 50,
+  },
+  roles: {
+    staff: process.env.ROLE_STAFF,
   },
 };
