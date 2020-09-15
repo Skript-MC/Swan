840dc50dd36f2a2d76ac38481bcac4e68156af99
Elliot Maisl <elliot.maisl@gmail.com>
2020-09-15 12:40:11 +0200

    ➕  Add mongoose

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 import SwanClient from './SwanClient';
 
 dotenv.config();
 
 const client = new SwanClient();
 client.login(process.env.DISCORD_TOKEN);
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+mongoose.connection.on('connected', () => {
+  client.logger.success('MongoDB is connected!');
+});
+mongoose.connection.on('error', (err) => {
+  client.logger.error('MongoDB connection error. Please make sure MongoDB is running.');
+  throw err;
+});
