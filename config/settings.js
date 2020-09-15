e273cf629069fc81f87f5a57a7dfd7efc415c9b9
Elliot Maisl <elliot.maisl@gmail.com>
2020-09-15 17:47:22 +0200

    ✨  Auto-react to the "Idea" channel

--- config/settings.js
+++ config/settings.js
@@ -8,13 +8,20 @@ export default {
     prefix: '.',
     avatar: path.join(__dirname, '..', 'assets', 'logo.png'),
   },
+  moderation: {
+    purgeLimit: 50,
+  },
+  emojis: {
+    yes: '755446079855526051',
+    no: '❌',
+  },
   colors: {
     default: '#4286f4',
   },
   roles: {
     staff: process.env.STAFF_ROLE,
   },
-  roles: {
-    staff: process.env.ROLE_STAFF,
+  channels: {
+    idea: process.env.IDEA_CHANNEL,
   },
 };
---
+++ src/listeners/client/message.js
@@ -0,0 +1,54 @@
+import { Listener } from 'discord-akairo';
+import { DMChannel, Permissions } from 'discord.js';
+import settings from '../../../config/settings';
+
+class MessageListener extends Listener {
+  constructor() {
+    super('message', {
+      event: 'message',
+      emitter: 'client',
+    });
+  }
+
+  async addReactionsInIdeaChannel(message) {
+    if (message.channel.id === settings.channels.idea) {
+      try {
+        await message.react(settings.emojis.yes);
+        await message.react(settings.emojis.no);
+      } catch (error) {
+        this.client.logger.error('Unable to add emojis to the idea channel.');
+        this.client.logger.detail(`Has "ADD_REACTION" permission: ${message.guild.me.permissionsIn(message.channel).has(Permissions.FLAGS.ADD_REACTIONS)}`);
+        this.client.logger.detail(`Emojis added: "${settings.emojis.yes}" + "${settings.emojis.no}"`);
+        this.client.logger.detail(`Idea channel ID/Current channel ID: ${settings.channels.idea}/${message.channel.id} (same=${settings.channels.idea === message.channel.id})`);
+        this.client.logger.detail(`Message: ${message.url}`);
+        this.client.logger.error(error.stack);
+      }
+    }
+    return false;
+  }
+
+  async* getTasks(message) {
+    yield await this.addReactionsInIdeaChannel(message);
+  }
+
+  async exec(message) {
+    const isCommand = this.client.commandHandler.parseWithPrefix(message, '=').command;
+    if (isCommand)
+      return;
+    if (message.author.bot || message.system || message.channel instanceof DMChannel)
+      return;
+
+    // Run all needed tasks, and stop either when there is no more tasks or
+    // when one returned true (= want to stop)
+    let next = { done: false };
+    const tasks = this.getTasks(message);
+
+    while (!next.done) {
+      if (next.value)
+        break;
+      next = await tasks.next(); // eslint-disable-line no-await-in-loop
+    }
+  }
+}
+
+export default MessageListener;
