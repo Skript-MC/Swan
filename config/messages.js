5e74d1e85e9a59e85c15b09c66be235e156d2f3f
Elliot Maisl <elliot.maisl@gmail.com>
2020-09-16 11:36:00 +0200

    ✨  Prevent active members from posting docs links
    This is to encourage them to fill the official documentation of
    Skript-MC, not to use others.

--- config/messages.js
+++ config/messages.js
@@ -3,6 +3,9 @@ export default {
     notAllowed: "Tu n'as pas la permission de faire cela :frowning:",
     insufficientClientPermissions: "Je n'ai pas les permissions Discord nécessaires pour éxecuter la commande {COMMAND}. (Permissions manquantes : {PERMISSIONS})",
   },
+  miscellaneous: {
+    noDocLink: "Tu ne peux pas poster ce lien ! Si la documentation de Skript-MC est incomplète, demande l'accès en écriture (à Vengelis) afin d'améliorer la documentation. Voici ton message, si tu souhaites le récupérer : ```\n{MESSAGE}\n```",
+  },
   prompt: {
     footer: '\n*Tapez "retour" pour annuler cette commande, et "stop" pour arrêter.*',
     timeout: 'Temps écoulé !',
