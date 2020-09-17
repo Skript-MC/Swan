747fea552f16879d67d39831066897cd8ab77976
Elliot Maisl <elliot.maisl@gmail.com>
2020-09-17 11:24:16 +0200

    💬 Highlight important words in command's descriptions

--- config/commands/moderation.js
+++ config/commands/moderation.js
@@ -13,7 +13,7 @@ export const purge = {
   },
   description: {
     name: 'Purge',
-    content: "Permet de supprimer plusieurs messages à la fois dans un salon, avec la possibilité de spécifier un membre en particulier. Il n'est possible que de supprimer des messages ayant été envoyés il y a moins de 15 jours : c'est une limitation de discord. Par défaut, les messages des membres du staff ne seront pas inclus. Si vous voulez aussi supprimer les messages du Staff, ajoutez le drapeau `--force` (ou `-f`)",
+    content: "Permet de __supprimer plusieurs messages à la fois__ dans un salon, avec la possibilité de spécifier un membre en particulier. Il n'est possible que de supprimer des messages ayant été envoyés il y a __moins de 15 jours__ : c'est une limitation de discord. Par défaut, les messages des membres du staff ne seront pas inclus. Si vous voulez aussi supprimer les messages du Staff, ajoutez le drapeau `--force` (ou `-f`)",
     usage: 'purge <nombre> [<@mention | pseudo | ID>] [--force | -f]',
     examples: ['purge 10 -f', 'purge @membre 40'],
     permissions: 'Staff',
