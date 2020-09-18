db6e9a039be1e0b25c9ca451f10fbbd5f36c88a7
Elliot Maisl <elliot.maisl@gmail.com>
2020-09-18 15:13:53 +0200

    ✨  Add an antispam in the "snippets" channel

--- config/messages.js
+++ config/messages.js
@@ -5,6 +5,7 @@ export default {
   },
   miscellaneous: {
     noDocLink: "Tu ne peux pas poster ce lien ! Si la documentation de Skript-MC est incomplète, demande l'accès en écriture (à Vengelis) afin d'améliorer la documentation. Voici ton message, si tu souhaites le récupérer : ```\n{MESSAGE}\n```",
+    noSpam: "Merci d'éviter les messages inutiles dans le salon des snippets. Ton message ne contient aucun bloc de code... Comment veux-tu partager ton script sans bloc de code ? Si tu ne sais pas comment faire, regarde ici : <https://support.discordapp.com/hc/fr/articles/210298617>.",
   },
   prompt: {
     footer: '\n*Tapez "retour" pour annuler cette commande, et "stop" pour arrêter.*',
