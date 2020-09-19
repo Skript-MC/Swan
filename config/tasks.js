5677883db28cba77c7fa80a86fcba75c9446f736
Romitou <romitou@protonmail.com>
2020-09-19 17:22:01 +0200

    :ok_hand: Updating code due to code review (thanks @noftaly)

--- config/tasks.js
+++ config/tasks.js
@@ -1,8 +1,7 @@
-export default {
-  skriptReleases: {
-    releaseAnnouncement: 'Une nouvelle version de Skript vient d\'être publiée ; vous pouvez la télécharger et consulter les changements ci-dessous.',
-    githubEndpoint: '/repos/SkriptLang/Skript/releases',
-    dataProvider: 'Données fournies par https://github.comol',
-    timeDifference: 600000,
-  },
+// eslint-disable-next-line import/prefer-default-export
+export const skriptReleases = {
+  releaseAnnouncement: 'Une nouvelle version de Skript vient d\'être publiée ; vous pouvez la télécharger et consulter les changements ci-dessous.',
+  githubEndpoint: '/repos/SkriptLang/Skript/releases',
+  dataProvider: 'Données fournies par https://github.com',
+  timeDifference: 600000,
 };
