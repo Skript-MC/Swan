dca79911200932bc5152fa2f521767d2e4496af5
Romitou <romitou@protonmail.com>
2020-09-19 16:55:46 +0200

    :sparkles: Introducing Skript Releases checks

---
+++ config/tasks.js
@@ -0,0 +1,8 @@
+export default {
+  skriptReleases: {
+    releaseAnnouncement: 'Une nouvelle version de Skript vient d\'être publiée ; vous pouvez la télécharger et consulter les changements ci-dessous.',
+    githubEndpoint: '/repos/SkriptLang/Skript/releases',
+    dataProvider: 'Données fournies par https://github.comol',
+    timeDifference: 600000,
+  },
+};
