ece687abf32b7bd31c4c889f77c475be059bf4e7
Elliot Maisl <elliot.maisl@gmail.com>
2020-09-19 23:06:13 +0200

    🚨 Disable "unicorn/prefer-replace-all"
    Because string#replaceAll is neither in NodeJS nor in babel yet...

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,10 @@ module.exports = {
   extends: 'noftalint',
   ignorePatterns: ['.eslintrc.js', 'node_modules/', 'build/'],
   reportUnusedDisableDirectives: true,
+  rules: {
+    // String#replaceAll is neither in NodeJS nor in babel yet...
+    'unicorn/prefer-replace-all': 'off',
+  },
   env: {
     node: true,
   },
