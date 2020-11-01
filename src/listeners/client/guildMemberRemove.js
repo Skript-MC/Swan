3058cdf3c48e2b52fa00bec9c271e1849e1c1fea
Elliot Maisl <elliot.maisl@gmail.com>
2020-11-01 14:06:25 +0100

    ✨  Add a hook with discord's native kick

--- src/listeners/client/guildMemberRemove.js
+++ src/listeners/client/guildMemberRemove.js
@@ -1,9 +1,11 @@
 import { Listener } from 'discord-akairo';
+import { GuildAuditLogs } from 'discord.js';
 import messages from '../../../config/messages';
 import settings from '../../../config/settings';
 import ModerationData from '../../structures/ModerationData';
 import ModerationHelper from '../../structures/ModerationHelper';
 import BanAction from '../../structures/actions/BanAction';
+import KickAction from '../../structures/actions/KickAction';
 import { constants } from '../../utils';
 
 class GuildMemberRemoveListener extends Listener {
@@ -18,6 +20,7 @@ class GuildMemberRemoveListener extends Listener {
     const isBanned = await ModerationHelper.isBanned(member.id, false);
 
     if (isBanned) {
+      // Check if they're leaving while being banned
       const channel = member.guild.channels.resolve(settings.channels.log);
       const data = new ModerationData(member.guild.me, member.guild, this.client, channel)
         .setVictim(member)
@@ -25,6 +28,21 @@ class GuildMemberRemoveListener extends Listener {
         .setReason(messages.moderation.reasons.leaveBan)
         .setType(constants.SANCTIONS.TYPES.HARDBAN);
       await new BanAction(data).commit();
+    } else {
+      // Check if they've been kicked
+      const kicks = await member.guild.fetchAuditLogs({ type: GuildAuditLogs.Actions.MEMBER_KICK });
+      const lastKick = kicks.entries.first();
+      if (lastKick
+        && lastKick.target.id === member.id
+        && !lastKick.executor.bot
+        && lastKick.createdTimestamp >= Date.now() - 1000) {
+        const channel = member.guild.channels.resolve(settings.channels.log);
+        const data = new ModerationData(member.guild.me, member.guild, this.client, channel)
+          .setVictim(member)
+          .setReason(lastKick.reason)
+          .setType(constants.SANCTIONS.TYPES.KICK);
+        await new KickAction(data).commit();
+      }
     }
   }
 }
