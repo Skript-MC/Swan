a5080d73fb943d976c7888549f1ff4fc98d2bd8c
Elliot Maisl <elliot.maisl@gmail.com>
2020-12-29 15:52:12 +0100

    💬 Improve readability by using line breaks instead of "\n"

--- config/messages.ts
+++ config/messages.ts
@@ -3,7 +3,9 @@ import { stripIndent } from 'common-tags';
 export default {
   global: {
     oops: process.env.NODE_ENV === 'production'
-      ? ":warning: Oups... Quelque chose s'est mal passé en réalisant cette action. Il se peut qu'elle ne se soit pas complètement terminée, voire pas commencée.\n> (Cc: <@188341077902753794>, <@191495299884122112>)"
+      ? stripIndent`
+        :warning: Oups... Quelque chose s'est mal passé en réalisant cette action. Il se peut qu'elle ne se soit pas complètement terminée, voire pas commencée.
+        > (Cc: <@188341077902753794>, <@191495299884122112>)`
       : ":warning: Oups... Quelque chose s'est mal passé en réalisant cette action. Il se peut qu'elle ne se soit pas complètement terminée, voire pas commencée.",
     notAllowed: "Tu n'as pas la permission de faire cela :frowning:",
     insufficientClientPermissions: "Je n'ai pas les permissions Discord nécessaires pour exécuter la commande {COMMAND}. (Permissions manquantes : {PERMISSIONS})",
@@ -14,12 +16,19 @@ export default {
     impossibleBecauseBanned: "Impossible d'effectuer cette action car le membre est banni.",
   },
   miscellaneous: {
-    noDocLink: "Tu ne peux pas poster ce lien ! Si la documentation de Skript-MC est incomplète, demande l'accès en écriture à Vengelis afin d'améliorer la documentation. Voici ton message, si tu souhaites le récupérer : ```\n{MESSAGE}\n```",
+    noDocLink: stripIndent`
+      Tu ne peux pas poster ce lien ! Si la documentation de Skript-MC est incomplète, demande l'accès en écriture à Vengelis afin d'améliorer la documentation.
+      Voici ton message, si tu souhaites le récupérer : \`\`\`
+      {MESSAGE}
+      \`\`\`
+    `,
     noSpam: "Merci d'éviter les messages inutiles dans le salon des snippets. Ton message ne contient aucun bloc de code... Comment veux-tu partager ton script sans bloc de code ? Si tu ne sais pas comment faire, regarde ici : <https://support.discord.com/hc/fr/articles/210298617>.",
     invalidMessage: 'Ton message ne respecte pas le règlement du salon {CHANNEL}, il a donc été supprimé. Tu peux réessayer, ou contacter un modérateur Discord pour en savoir plus.',
     ghostPingSingular: ':warning: {MENTIONS}, tu as été ghost-ping par {MEMBER}. :innocent:',
     ghostPingPlural: ':warning: {MENTIONS}, vous avez été ghost-ping par {MEMBER}. :innocent:',
-    renamed: "Bienvenue sur le serveur Discord de Skript-MC !\nTon pseudonyme contenait des caractères spéciaux, donc je me suis permis de te les retirer (voire de te renommer complètement), pour que ce soit plus simple de te mentionner. J'espère que mes modifications te plairont.\nSi malheureusement ce n'est pas le cas, tu peux demander à un modérateur Discord (pseudonymes vert clair) pour être renommé.",
+    renamed: stripIndent`
+      Ton pseudonyme contenait des caractères spéciaux, donc je me suis permis de te les retirer (voire de te renommer complètement), pour que ce soit plus simple de te mentionner. J'espère que mes modifications te plairont.
+      Si malheureusement ce n'est pas le cas, tu peux demander à un modérateur Discord (pseudonymes vert clair) pour être renommé.`,
     renameList: [
       'Arachnoïde',
       'Banjo',
