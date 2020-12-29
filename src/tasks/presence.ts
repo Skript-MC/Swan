9ff9e17eb9cda81331d529884ce32774c93015c3
Elliot Maisl <elliot.maisl@gmail.com>
2020-12-29 15:50:10 +0100

    🔥 Remove unnecessary TODO

--- src/tasks/presence.ts
+++ src/tasks/presence.ts
@@ -19,7 +19,6 @@ class PresenceTask extends Task {
 
   private * _getActivity(): Generator<PresenceData, never> {
     while (true) {
-      // TODO: Move this in config/settings.js?
       yield { activity: { name: `${this.client.guild.memberCount} membres 🎉`, type: 'WATCHING' }, status: 'online' };
       yield { activity: { name: `${settings.bot.prefix}aide | Skript-MC`, type: 'WATCHING' }, status: 'online' };
     }
