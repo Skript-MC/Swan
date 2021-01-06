acf7babd94b6dcf8bdd2aab621f3846035417baa
BakaAless <44125445+BakaAless@users.noreply.github.com>
2021-01-06 16:21:16 +0100

    Add an argument
    Add an argument to change the lines number
    Ajout d'un argument pour changer le numéro des lignes

--- src/commands/basic/code.js
+++ src/commands/basic/code.js
@@ -5,13 +5,33 @@ class Code extends Command {
   constructor() {
     super('Code');
     this.aliases = ['code', 'balise'];
-    this.usage = 'code [-l] <votre code>';
+    this.usage = 'code [-l [-s <entier>]] <votre code>';
     this.examples = ['code broadcast "Yeah!"', 'code -l on join: message "salut !"'];
   }
 
   async execute(_client, message, args) {
+    
     const printLines = args[0] === '-l';
-    if (printLines) args.shift();
+    let startAtLine = 0;
+    
+    if (printLines) {
+      args.shift();
+      if (args[0] === "-s") {
+        args.shift();
+        const value = args.shift();
+        if (this.isNumeric(value)) {
+          // On enlève 1 car il est rajouté plus tard
+          startAtLine = parseInt(value) - 1;
+          // Pour éviter des p'tits malins
+          if (startAtLine < 0) startAtLine = 0;
+        } else {
+          // Si c'est pas un nombre après l'argument s on le réinjecte
+          // dans le tableau d'argument, au début
+          args.reverse().push(value)
+          args.reverse();
+        }
+      }
+    }
 
     let code = args.join(' ');
     if (code.length === 0) return message.channel.send(this.config.noCode);
@@ -22,8 +42,8 @@ class Code extends Command {
       const lines = code.split('\n');
       code = '';
       for (const [i, line] of lines.entries()) {
-        const space = lines.length.toString().length - (i + 1).toString().length;
-        code += `\n${i + 1}${' '.repeat(space)} | ${line}`;
+        const space = (startAtLine + lines.length).toString().length - (startAtLine + i + 1).toString().length;
+        code += `\n${startAtLine + i + 1}${' '.repeat(space)} | ${line}`;
       }
     }
 
@@ -47,6 +67,14 @@ class Code extends Command {
         collector.stop();
       });
   }
+
+  isNumeric(str) {
+    // On vérifie que ce soit bien un string
+    if (typeof str != "string") return false;
+    // On vérifie qu'il ne soit pas null et que le parseInt non plus
+    return !isNaN(str) && !isNaN(parseInt(str))
+  }
+
 }
 
 export default Code;
