e710637df6e9432684b32860f5c7b789e948b4b3
BakaAless <44125445+BakaAless@users.noreply.github.com>
2021-01-06 16:42:22 +0100

    Fix ESLint rules
    Respect des règles ESLint

--- src/commands/basic/code.js
+++ src/commands/basic/code.js
@@ -8,26 +8,23 @@ class Code extends Command {
     this.usage = 'code [-l [-s <entier>]] <votre code>';
     this.examples = ['code broadcast "Yeah!"', 'code -l on join: message "salut !"'];
   }
-
   async execute(_client, message, args) {
-    
     const printLines = args[0] === '-l';
     let startAtLine = 0;
-    
     if (printLines) {
       args.shift();
-      if (args[0] === "-s") {
+      if (args[0] === '-s') {
         args.shift();
         const value = args.shift();
         if (this.isNumeric(value)) {
           // On enlève 1 car il est rajouté plus tard
-          startAtLine = parseInt(value) - 1;
+          startAtLine = parseInt(value, 10) - 1;
           // Pour éviter des p'tits malins
           if (startAtLine < 0) startAtLine = 0;
         } else {
           // Si c'est pas un nombre après l'argument s on le réinjecte
           // dans le tableau d'argument, au début
-          args.reverse().push(value)
+          args.reverse().push(value);
           args.reverse();
         }
       }
@@ -67,14 +64,12 @@ class Code extends Command {
         collector.stop();
       });
   }
-
   isNumeric(str) {
     // On vérifie que ce soit bien un string
-    if (typeof str != "string") return false;
+    if (typeof str !== 'string') return false;
     // On vérifie qu'il ne soit pas null et que le parseInt non plus
-    return !isNaN(str) && !isNaN(parseInt(str))
+    return !isNaN(str) && !isNaN(parseInt(str), 10);
   }
-
 }
 
 export default Code;
