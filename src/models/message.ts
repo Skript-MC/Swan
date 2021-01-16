e47d6d1066ef37f122f86d8cad142752851ccdfb
Romain <romitou@protonmail.com>
2021-01-17 00:03:49 +0100

    ✨Add panel-controlled messages
    Ajout des messages (#126)
    
    * :sparkles: Introducing errorDetails command
    
    * :sparkles: Improving code due to code reviews (thanks to noftaly)
    
    * :sparkles: Introducing addonPack and autoMessage commands

---
+++ src/models/message.ts
@@ -0,0 +1,23 @@
+import { model, Schema } from 'mongoose';
+import type { MessageDocument, MessageModel } from '../types';
+
+const MessageSchema = new Schema({
+  messageType: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  aliases: {
+    type: [String],
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+});
+
+export default model<MessageDocument, MessageModel>('Message', MessageSchema);
---
+++ src/utils/searchMessageSimilarity.ts
@@ -0,0 +1,19 @@
+import type { MessageDocument } from '../types';
+import { jaroWinklerDistance } from './index';
+
+function searchMessageSimilarity(entries: MessageDocument[], wanted: string): MessageDocument {
+  const search: Array<[message: MessageDocument, similarity: number]> = [];
+  for (const entry of entries) {
+    // Avoid useless double loop after
+    if (entry.aliases.some(alias => alias === wanted) || entry.name === wanted) return entry;
+    for (const alias of entry.aliases) {
+      const distance = jaroWinklerDistance(alias, wanted);
+      if (distance >= 0.7) search.push([entry, distance]);
+    }
+  }
+  search.sort((a, b) => b[1] - a[1]);
+  if (search.length === 0) return null;
+  return search[0][0];
+}
+
+export default searchMessageSimilarity;
