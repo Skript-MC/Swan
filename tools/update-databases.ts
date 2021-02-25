572bf1cbfdb84377f50e233a6232f037a3a0ec6b
Elliot Maisl <elliot.maisl@gmail.com>
2021-02-25 12:58:45 +0100

    🗃  Add a migration for CommandStats

--- tools/update-databases.ts
+++ tools/update-databases.ts
@@ -1,8 +1,11 @@
+import 'source-map-support/register';
+import 'module-alias/register';
 import 'dotenv/config';
 
 import { promises as fs } from 'fs';
 import path from 'path';
 import mongoose from 'mongoose';
+import CommandStat from '../src/models/commandStat';
 import ConvictedUser from '../src/models/convictedUser';
 import Sanction from '../src/models/sanction';
 
@@ -35,7 +38,38 @@ function getNewUpdateType(type: string): 'duration' | 'revoked' {
   }
 }
 
-export interface SanctionNedbSchema {
+const commandMap = new Map([
+  ['8ball', 'eightBall'],
+  ['Addon Info', 'addonInfo'],
+  ['Addon Pack', 'addonPack'],
+  ['Automatic Messages', 'autoMessage'],
+  ['Ban', 'ban'],
+  ['Code', 'code'],
+  ['Discover', 'discover'],
+  ['Error Details', 'errorDetails'],
+  ['Help', 'help'],
+  ['History', 'history'],
+  ['Idée', 'idea'],
+  ['Joke', 'joke'],
+  ['Kick', 'kick'],
+  ['Links', 'links'],
+  ['Move Message', 'move'],
+  ['Mute', 'mute'],
+  ['Ping', 'ping'],
+  ['Poll', 'poll'],
+  ['Purge', 'purge'],
+  ['Remove Warn', 'removeWarn'],
+  ['Server Info', 'serverInfo'],
+  ['Skript Info', 'skriptInfo'],
+  ['Statistics', 'statistics'],
+  ['Syntax Info', 'documentation'],
+  ['Unban', 'unban'],
+  ['Unmute', 'unmute'],
+  ['User Info', 'userInfo'],
+  ['Warn', 'warn'],
+]);
+
+interface SanctionNedbSchema {
   memberId: string;
   sanctions: Array<{
     id: string;
@@ -60,12 +94,20 @@ export interface SanctionNedbSchema {
   lastMuteId: string | null;
 }
 
-void (async (): Promise<void> => {
-  console.log('Starting conversion of sanction databases from v1 to v2...');
+interface CommandStatsNedbSchema {
+  command: '8ball' | 'Addon Info' | 'Addon Pack' | 'Automatic Messages' | 'Ban' | 'Bassboost' | 'Change Config' | 'Code' | 'Discover' | 'Edit Messages' | 'Error Details' | 'Ghostping' | 'Help' | 'History' | 'Idée' | 'Join' | 'Joke' | 'Kick' | 'Leave' | 'Links' | 'Loop' | 'Move Message' | 'Mute' | 'Now Playing' | 'Pause' | 'Ping' | 'Play' | 'Player Info' | 'Poll' | 'Purge' | 'Queue' | 'Ranking' | 'Remove Warn' | 'Rules' | 'Server Info' | 'Shuffle' | 'Skip' | 'Skript Info' | 'Statistics' | 'Status' | 'Stop' | 'Tag Role' | 'Toggle role notification' | 'Unban' | 'Unmute' | 'User Info' | 'Volume' | 'Warn';
+  used: number;
+  _id: string;
+}
 
-  const rawContent = await fs.readFile(path.join(__dirname, '..', '..', 'databases', 'sanctionsHistory.db'));
-  const parsableContent = `[${rawContent}]`;
-  const inputArray: SanctionNedbSchema[] = JSON.parse(parsableContent);
+void (async (): Promise<void> => {
+  console.log('[0/2] Connecting to database...');
+
+  const rawSanctionContent = await fs.readFile(path.join(__dirname, '..', '..', 'databases', 'sanctionsHistory.db'));
+  const sanctionInputArray: SanctionNedbSchema[] = JSON.parse(`[${rawSanctionContent}]`);
+
+  const rawCommandStatsContent = await fs.readFile(path.join(__dirname, '..', '..', 'databases', 'commandStats.db'));
+  const commandStatsInputArray: CommandStatsNedbSchema[] = JSON.parse(`[${rawCommandStatsContent}]`);
 
   await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -81,10 +123,11 @@ void (async (): Promise<void> => {
     throw err;
   });
 
+  console.log('[1/2] Starting conversion of sanction databases...');
 
   // Iterate through all the old "histories". We don't care about the old "sanctions" database because it
   // only contains sanctions happening currently, whereas history contains all the sanction...
-  for (const [i, document] of inputArray.entries()) {
+  for (const [i, document] of sanctionInputArray.entries()) {
     // Add the user to the database of the convicted users (= users that have had at least 1 sanction).
     const { _id: userId } = await ConvictedUser.create({
       memberId: document.memberId,
@@ -134,12 +177,30 @@ void (async (): Promise<void> => {
     }
     // Draw the progress bar
     const dots = '.'.repeat(i);
-    const left = inputArray.length - i;
+    const left = sanctionInputArray.length - i;
     const empty = ' '.repeat(left);
-    process.stdout.write(`\r[${dots}${empty}]   |   ${i + 1}/${inputArray.length}${i + 1 === inputArray.length ? '\n' : ''}`);
+    process.stdout.write(`\r[${dots}${empty}]   |   ${i + 1}/${sanctionInputArray.length}${i + 1 === sanctionInputArray.length ? '\n' : ''}`);
   }
 
-  console.log('Conversion was successful! You can now check your mongodb database to verify that everything is good, and you can delete the old database (./databases/sanctions.db and ./databases/sanctionsHistory.db)');
+  console.log('\n[2/2] Starting conversion of command database...');
+
+  for (const [i, document] of commandStatsInputArray.entries()) {
+    const commandId = commandMap.get(document.command);
+    if (commandId) {
+      await CommandStat.create({
+        commandId,
+        uses: document.used,
+      });
+    }
+
+    // Draw the progress bar
+    const dots = '.'.repeat(i);
+    const left = commandStatsInputArray.length - i;
+    const empty = ' '.repeat(left);
+    process.stdout.write(`\r[${dots}${empty}]   |   ${i + 1}/${commandStatsInputArray.length}`);
+  }
+
+  console.log('\nConversion was successful! You can now check your MongoDB database to verify that everything is good, and you can delete the old databases (./databases/sanctions.db, ./databases/sanctionsHistory.db, ./databases/commandStats.db)');
   // eslint-disable-next-line node/no-process-exit
   process.exit(0);
 })();
