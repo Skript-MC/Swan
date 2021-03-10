f92afbed04a4c9f76f36df4c96157022ce2024e3
Skylyxx <skylyxxfr@gmail.com>
2021-03-10 14:05:38 +0100

    Simplify emote type

--- src/resolvers/emote.ts
+++ src/resolvers/emote.ts
@@ -3,12 +3,9 @@ import settings from '@/conf/settings';
 import type { GuildMessage } from '../types';
 
 export default function emote(message: GuildMessage, phrase: string): string | null {
-  if (typeof phrase === 'undefined')
+  if (typeof phrase === 'undefined' || phrase === '')
     phrase = settings.emojis.yes;
   if (nodeEmoji.hasEmoji(phrase))
     return nodeEmoji.find(phrase).emoji;
-  const splitted = phrase.split(':');
-  const id = splitted[splitted.length - 1].split('>')[0];
-  const emoji = message.guild.emojis.cache.get(id);
-  return emoji?.toString();
+  return message.client.util.resolveEmoji(phrase, message.guild.emojis.cache)?.toString();
 }
