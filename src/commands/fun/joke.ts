1f8c75d6fb23a09291a4c190384104c91b09068c
Romitou <romitou@protonmail.com>
2021-03-13 23:50:56 +0100

    :sparkles: Migration of jokes in database

--- src/commands/fun/joke.ts
+++ src/commands/fun/joke.ts
@@ -1,7 +1,9 @@
 import { Command } from 'discord-akairo';
 import { MessageEmbed } from 'discord.js';
 import pupa from 'pupa';
+import Message from '@/app/models/message';
 import type { GuildMessage } from '@/app/types';
+import { MessageName } from '@/app/types';
 import type { JokeCommandArguments } from '@/app/types/CommandArguments';
 import { joke as config } from '@/conf/commands/fun';
 import messages from '@/conf/messages';
@@ -12,18 +14,35 @@ class JokeCommand extends Command {
     super('joke', {
       aliases: config.settings.aliases,
       details: config.details,
+      args: [{
+        id: 'jokeName',
+        type: 'string',
+        match: 'content',
+      }],
       clientPermissions: config.settings.clientPermissions,
       userPermissions: config.settings.userPermissions,
       channel: 'guild',
     });
   }
 
-  public async exec(message: GuildMessage, _args: JokeCommandArguments): Promise<void> {
-    const joke = config.messages.jokes[Math.floor(Math.random() * config.messages.jokes.length)];
+  public async exec(message: GuildMessage, args: JokeCommandArguments): Promise<void> {
+    console.log(args);
+    let joke;
+    if (args.jokeName) {
+      joke = await Message.findOne({ aliases: args.jokeName, messageType: MessageName.Joke });
+    } else {
+      const jokeCount = await Message.count({ messageType: MessageName.Joke });
+      const random = Math.floor(Math.random() * jokeCount);
+      joke = await Message.findOne({ messageType: MessageName.Joke }).skip(random);
+    }
+
+    if (!joke?.content) {
+      await message.channel.send(config.messages.notFound);
+      return;
+    }
 
     const embed = new MessageEmbed()
-      .setTitle(`:small_blue_diamond: ${joke.split(';')[0]}`)
-      .setDescription(joke.split(';')[1])
+      .setDescription(joke.content)
       .setColor(settings.colors.default)
       .setFooter(pupa(messages.global.executedBy, { member: message.member }))
       .setTimestamp();
