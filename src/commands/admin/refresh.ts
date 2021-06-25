3fdb97379e79f5b7d190455691fea50e341d9426
Romitou <romitou@protonmail.com>
2021-06-25 20:33:47 +0200

    :sparkles: Add toggle module command

--- src/commands/admin/refresh.ts
+++ src/commands/admin/refresh.ts
@@ -1,11 +1,10 @@
-import type { AkairoHandler } from 'discord-akairo';
 import { Command } from 'discord-akairo';
 import SharedConfig from '@/app/models/sharedConfig';
 import SwanModule from '@/app/models/swanModule';
 import type { GuildMessage, SharedConfigDocument } from '@/app/types';
 import { SharedConfigName } from '@/app/types';
 import type { RefreshCommandArgument } from '@/app/types/CommandArguments';
-import { nullop } from '@/app/utils';
+import { nullop, toggleModule } from '@/app/utils';
 import { refresh as config } from '@/conf/commands/admin';
 
 class RefreshCommand extends Command {
@@ -22,21 +21,8 @@ class RefreshCommand extends Command {
   public async exec(message: GuildMessage, _args: RefreshCommandArgument): Promise<void> {
     // Refresh modules
     const modules = await SwanModule.find();
-    for (const module of modules) {
-      const handler: AkairoHandler = this.client[module.handler];
-      const cachedModule = this.client.cache.modules.find(mod => mod.id === module.name);
-      if (!cachedModule)
-        continue;
-      // See if the module is present in handler.modules (= if it is loaded).x
-      const currentState = Boolean(handler.modules.findKey((_, key) => key === cachedModule.id));
-
-      if (handler && module.enabled !== currentState) {
-        if (module.enabled)
-          handler.load(cachedModule.filepath);
-        else
-          handler.remove(module.name);
-      }
-    }
+    for (const module of modules)
+      toggleModule(this.client, module, module.enabled);
 
     // Refresh saved channels
     const configDocument: SharedConfigDocument = await SharedConfig.findOne({
---
+++ src/utils/toggleModule.ts
@@ -0,0 +1,18 @@
+import type { AkairoClient, AkairoHandler } from 'discord-akairo';
+import type { SwanModuleDocument } from '@/app/types';
+
+export default function toggleModule(client: AkairoClient, module: SwanModuleDocument, isEnabled: boolean): void {
+  const handler: AkairoHandler = client[module.handler];
+  const cachedModule = client.cache.modules.find(mod => mod.id === module.name);
+  if (!cachedModule)
+    return;
+  // See if the module is present in handler.modules (= if it is loaded).
+  const currentState = Boolean(handler.modules.findKey((_, key) => key === cachedModule.id));
+
+  if (handler && isEnabled !== currentState) {
+    if (isEnabled)
+      handler.load(cachedModule.filepath);
+    else
+      handler.remove(module.name);
+  }
+}
