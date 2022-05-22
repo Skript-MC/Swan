c3b1b634b7f392b56e0cbbe8c38f0c93f9072cd7
Romitou <romitou@protonmail.com>
2022-05-22 17:37:24 +0200

    :goal_net: Add more context to Sentry exceptions

--- src/listeners/commands/contextMenuCommandError.ts
+++ src/listeners/commands/contextMenuCommandError.ts
@@ -7,7 +7,13 @@ import messages from '@/conf/messages';
 export default class CommandErrorListener extends Listener<typeof Events.ContextMenuCommandError> {
   public override async run(error: Error, { interaction }: ContextMenuCommandErrorPayload): Promise<void> {
     await interaction.reply(messages.global.oops).catch(noop);
-    captureException(error);
+    captureException(error, {
+      user: interaction.user,
+      extra: {
+        command: interaction.command.toJSON(),
+        stacktrace: error.stack,
+      },
+    });
     this.container.logger.error('Oops, something went wrong with a command!');
     this.container.logger.info(`Command: ${interaction.commandName}`);
 
