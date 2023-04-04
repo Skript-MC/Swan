a4640d82fb3a2d5604e2ad7d062c68c751eb6d36
Romitou <romitou@protonmail.com>
2023-04-04 19:20:02 +0200

    :bug: Remove old mute logics

--- src/listeners/client/guildMemberAdd.ts
+++ src/listeners/client/guildMemberAdd.ts
@@ -2,7 +2,6 @@ import { Listener } from '@sapphire/framework';
 import type { GuildMember } from 'discord.js';
 import { PermissionsBitField } from 'discord.js';
 import pupa from 'pupa';
-import ModerationHelper from '@/app/moderation/ModerationHelper';
 import { noop, toValidName } from '@/app/utils';
 import messages from '@/conf/messages';
 import settings from '@/conf/settings';
@@ -10,7 +9,6 @@ import settings from '@/conf/settings';
 export default class GuildMemberAddListener extends Listener {
   public override async run(member: GuildMember): Promise<void> {
     await this._greet(member);
-    await this._remute(member);
     await this._rename(member);
   }
 
@@ -22,22 +20,6 @@ export default class GuildMemberAddListener extends Listener {
 
     const content = pupa(randomMessage, { member });
     await channel.send(content).catch(noop);
-  }
-
-  private async _remute(member: GuildMember): Promise<void> {
-    const currentMute = await ModerationHelper.getCurrentMute(member.id);
-    if (currentMute) {
-      try {
-        const muteRole = member.guild.roles.resolve(settings.roles.mute);
-        if (muteRole)
-          await member.roles.add(muteRole);
-      } catch (unknownError: unknown) {
-        this.container.logger.error('Could not add the mute role to a member.');
-        this.container.logger.info(`MuteObject: "${JSON.stringify(currentMute)}"`);
-        this.container.logger.info(`Manager roles permission: ${member.guild.members.me?.permissions.has(PermissionsBitField.Flags.ManageRoles)}`);
-        this.container.logger.error((unknownError as Error).stack);
-      }
-    }
   }
 
   private async _rename(member: GuildMember): Promise<void> {
