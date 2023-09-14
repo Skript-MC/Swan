1d098937ddee75c13c538bad99d9ece5c6ff103c
Elliot Maisl <elliot.maisl@gmail.com>
2023-09-14 10:39:51 +0200

    ♻️ Switch tools to JS for easier bundling/running

--- tools/remove-commands.js
+++ tools/remove-commands.js
@@ -1,16 +1,20 @@
-import 'source-map-support/register';
-import 'module-alias/register';
 import 'dotenv/config';
-
 import { Client, GatewayIntentBits } from 'discord.js';
 
-async function start(): Promise<void> {
+async function start() {
   const client = new Client({
     intents: [
       GatewayIntentBits.GuildIntegrations,
     ],
   });
   await client.login(process.env.DISCORD_TOKEN);
+
+  // Await for the client to be ready.
+  await new Promise((resolve) => {
+    client.once('ready', () => {
+      resolve();
+    });
+  });
 
   console.log('Removing commands...');
   await client.application.commands.set([]);
--- tsup.config.ts
+++ tsup.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from 'tsup';
 /* eslint-disable-next-line import/no-default-export */
 export default defineConfig({
   clean: true,
+  silent: true,
   dts: false,
-  entry: ['src/**/*.ts', '!src/**/*.d.ts', 'tools/**/*.ts', 'config/**/*.ts'],
+  entry: ['src/**/*.ts', '!src/**/*.d.ts', 'config/**/*.ts'],
   format: ['esm'],
   minify: false,
   skipNodeModulesBundle: true,
