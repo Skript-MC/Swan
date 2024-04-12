7de9ddda9b6fb27aa541e292f8d007de544e1e14
Romitou <romitou@protonmail.com>
2024-04-12 18:29:06 +0200

    :fire: Remove poll feature

--- src/listeners/client/messageReactionAdd.ts
+++ src/listeners/client/messageReactionAdd.ts
@@ -1,25 +1,15 @@
+import { channels, emojis } from '#config/settings';
+import { ReactionRole } from '#models/reactionRole';
+import type { GuildMessage } from '#types/index';
 import { Listener } from '@sapphire/framework';
 import type { MessageReaction, User } from 'discord.js';
-import pupa from 'pupa';
-import * as messages from '#config/messages';
-import { channels, emojis, miscellaneous } from '#config/settings';
-import { Poll } from '#models/poll';
-import { ReactionRole } from '#models/reactionRole';
-import * as PollManager from '#structures/PollManager';
-import type { GuildMessage } from '#types/index';
-import { QuestionType } from '#types/index';
-import { noop, nullop } from '#utils/index';
-
-type PollAnswer = [reactionName: string, votersIds: string[]];
 
 export class MessageReactionAddListener extends Listener {
   public override async run(reaction: MessageReaction, user: User): Promise<void> {
     if (user.bot || !reaction.message.inGuild())
       return;
 
-    if (this.container.client.cache.pollMessagesIds.has(reaction.message.id))
-      await this._handlePoll(reaction, reaction.message, user);
-    else if (channels.idea === reaction.message.channel.id)
+    if (channels.idea === reaction.message.channel.id)
       await this._handleSuggestion(reaction, reaction.message, user);
     else if (this.container.client.cache.reactionRolesIds.has(reaction.message.id))
       await this._handleReactionRole(reaction, reaction.message, user);
@@ -39,79 +29,6 @@ export class MessageReactionAddListener extends Listener {
       const reactors = message.reactions.cache.find(r => reactionIs(r, emojis.yes))?.users;
       if (reactors?.cache.get(user.id))
         await reactors.remove(user);
-    }
-  }
-
-  private async _handlePoll({ emoji, users }: MessageReaction, message: GuildMessage, user: User): Promise<void> {
-    const poll = await Poll.findOne({ messageId: message.id });
-    if (!poll)
-      return;
-
-    const { pollReactions } = miscellaneous;
-
-    // Whether they react with the appropriate "answer reaction" for this poll
-    if ((poll.questionType === QuestionType.Yesno && pollReactions.yesno.includes(emoji.name!))
-      || (poll.questionType === QuestionType.Choice && pollReactions.multiple.includes(emoji.name!))) {
-      // Find the reaction they chose before (undefined if they never answered).
-      const previousUserVote = Object.entries(poll.votes)
-        // We find all the entries where the user id is in the votersIds array.
-        .find((entry: PollAnswer) => (entry[1].includes(user.id) ? entry : null))
-        // We take the reactionName if it exists.
-        ?.[0];
-
-      if (previousUserVote === emoji.name) {
-        // If they already voted for this option
-        const infoMessage = await message.channel.send(messages.poll.alreadyVoted);
-        setTimeout(async () => {
-          if (infoMessage.deletable)
-            await infoMessage.delete().catch(noop);
-        }, 5000);
-      } else if (previousUserVote) {
-        // They have already voted, but they want to change
-        // TODO: Support the "poll.multiple" option
-        await Poll.findByIdAndUpdate(poll._id, {
-          $pull: { [`votes.${previousUserVote}`]: user.id },
-          $push: { [`votes.${emoji.name}`]: user.id },
-        });
-      } else {
-        // If they want to vote, and have never done so
-        await Poll.findByIdAndUpdate(
-          poll._id,
-          { $push: { [`votes.${emoji.name}`]: user.id } },
-        );
-      }
-
-      await Promise.allSettled(
-        message.reactions.cache
-          .filter(r => r.users.cache.has(user.id) && r.emoji.name !== emoji.name)
-          .map(async r => r.users.remove(user.id)),
-      );
-      if (poll.anonymous)
-        await users.remove(user);
-    } else if (pollReactions.specials[1] === emoji.name && user.id === poll.memberId) {
-      // If the poll's creator clicked the "Stop" button
-      await PollManager.end(poll.id as string, true);
-    } else if (pollReactions.specials[0] === emoji.name) {
-      // If someone clicked the "Info" button
-      await users.remove(user);
-      const member = message.guild.members.resolve(user.id);
-
-      try {
-        if (member) {
-          const text = poll.questionType === QuestionType.Yesno
-            ? messages.poll.informationsYesNo
-            : messages.poll.informationsCustom;
-          await member.send(text);
-
-          const infoMessage = await message.channel.send(pupa(messages.poll.dmSent, { member })).catch(nullop);
-          setTimeout(async () => {
-            if (infoMessage?.deletable)
-              await infoMessage.delete().catch(noop);
-          }, 5000);
-        }
-      } catch {
-        await message.channel.send(pupa(messages.global.dmAreClosed, { member }));
-      }
     }
   }
 
